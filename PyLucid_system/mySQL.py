ed1f746a4e3a378b15aae5e81a4a48cb10619c64
pylucid <pylucid@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2005-12-09 15:14:12 +0000

    ein wenig Aufgeräumt

--- PyLucid_system/mySQL.py
+++ PyLucid_system/mySQL.py
@@ -81,18 +81,6 @@ def error( msg, e ):
     sys.exit(0)
 
 
-#~ def dict_factory(cursor, row):
-    #~ d = {}
-    #~ for idx, col in enumerate(cursor.description):
-        #~ d[col[0]] = row[idx]
-    #~ return d
-
-#~ class DictCursor(dbapi.Cursor):
-    #~ def __init__(self, *args, **kwargs):
-        #~ dbapi.Cursor.__init__(self, *args, **kwargs)
-        #~ self.row_factory = dict_factory
-
-
 
 class mySQL:
     """
