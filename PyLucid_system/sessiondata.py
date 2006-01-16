bbb9a2bc48748f5eeda656232095d617724289b9
pylucid <pylucid@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2006-01-16 15:44:54 +0000

    - Code cleanup     - CGIdata used UserDict

--- PyLucid_system/sessiondata.py
+++ PyLucid_system/sessiondata.py
@@ -3,11 +3,12 @@
 
 
 
-__version__ = "v0.0.6"
+__version__ = "v0.1"
 
 __history__ = """
-v0.0.6
-
+v0.1
+    - Code cleanup
+    - CGIdata used UserDict
 v0.0.5
     - Fehlerabfrage bei convert_types() erweitert
 v0.0.4
@@ -23,39 +24,32 @@ v0.0.1
 
 
 # Python-Basis Module einbinden
-import os, cgi, urllib
+import os, sys, cgi, urllib, UserDict
+from socket import getfqdn
+
 
 # PyLucid Module
 import config
 
+
 if config.system.page_msg_debug:
     import inspect
 
-from socket import getfqdn
-
-#~ from config import dbconf
 
 
-# Für Debug-print-Ausgaben
-#~ print "Content-type: text/html\n\n<pre>%s</pre>" % __file__
-#~ print "<pre>"
 
-import sys
-
-class CGIdata:
+class CGIdata(UserDict.UserDict):
     """
-    Wertet die POST- und GET-Daten aus.
-    Macht sich als dict verfügbar.
-    Stellt fest, welche Seite abgefunden werden soll
+    Wertet die POST- und GET-Daten aus. Macht diese als dict verfügbar.
+    Dabei werden alle Zahlen mit int() konvertiert.
+    Fügt in's dict noch client_ip und client_domain ein (Methode get_client_info)
     """
-    def __init__( self, PyLucid ):
+    def __init__(self, PyLucid):
         """
         CGIdata ist eine abgeleitetes Dictionary und kann
         somit wie ein Dict angesprochen werden.
         """
         self.page_msg   = PyLucid["page_msg"]
-
-        #~ self.page_msg( "TEST*****" )
 
         self.data = {} # Dict in dem die CGIdaten gespeichert werden
 
@@ -63,9 +57,9 @@ class CGIdata:
 
         self.convert_types()
 
-        self.get_client_info()
+        self.get_client_info() # Client Info's ins CGI-data-dict packen
 
-    def get_CGIdata( self ):
+    def get_CGIdata(self):
         """
         sammelt POST und GET Daten zusammen
         Er wird zuerst cgi.FieldStorage abgefragt und dann erst den QUERY_STRING, weil
@@ -73,7 +67,7 @@ class CGIdata:
         Daten gebraucht werden, die im QUERY_STRING noch drin stecken.
         Das ist wichtig für Seitennamen mit "Sonderzeichen" wie "/" ;)
 
-        Normalerweise reicht ein cgi.FieldStorage( keep_blank_values=1 ) und die
+        Normalerweise reicht ein cgi.FieldStorage(keep_blank_values=1) und die
         os.environ['QUERY_STRING'] Auswertung könnte man sich sparen. Aber das
         ganze funktioniert nicht mit Python v2.2.1 :( Also wird's doch umständlich
         gemacht ;)
@@ -88,7 +82,7 @@ class CGIdata:
                 print "Content length = %s" % length
                 sys.exit()
             #~ else:
-                #~ self.page_msg( "Content length = %s" % length )
+                #~ self.page_msg("Content length = %s" % length)
 
             FieldStorageData = cgi.FieldStorage()
 
@@ -101,7 +95,7 @@ class CGIdata:
             for item in os.environ['QUERY_STRING'].split("&"):
                 size = len(item)
                 if size>10000:
-                    self.page_msg( "CGI Error, GET Parameter size overload: '%s...'" % item[:10])
+                    self.page_msg("CGI Error, GET Parameter size overload: '%s...'" % item[:10])
                     continue
 
                 item = item.split("=",1)
@@ -111,20 +105,20 @@ class CGIdata:
                 else:
                     self.data[ item[0] ] = item[1]
 
-        #~ self.page_msg( self.data )
+        #~ self.page_msg(self.data)
 
-    def convert_types( self ):
+    def convert_types(self):
         """
         Versucht Zahlen von str nach int zu convertieren
         """
         for k,v in self.data.iteritems():
             try:
-                self.data[k] = int( v )
+                self.data[k] = int(v)
             except:
                 pass
 
 
-    def get_client_info( self ):
+    def get_client_info(self):
         """
         Client Informationen festhalten. Ist u.a. für's SQL-logging interessant.
         """
@@ -135,80 +129,46 @@ class CGIdata:
 
         self.data["client_ip"] = os.environ["REMOTE_ADDR"]
         try:
-            self.data["client_domain"] = getfqdn( self.data["client_ip"] )
+            self.data["client_domain"] = getfqdn(self.data["client_ip"])
         except Exception, e:
             self.data["client_domain"] = "unknown: '%s'" % e
 
     #______________________________________________________________________________
-    # Methoden um an die Daten zu kommen ;)
 
-    def __getitem__( self, key ):
-        return self.data[key]
-
-    def iteritems( self ):
-        return self.data.iteritems()
-
-    def __setitem__( self, key, value ):
-        self.data[key] = value
-
-    def has_key( self, key ):
-        return self.data.has_key( key )
-
-    def __str__( self ):
-        return str( self.data )
-
-    def keys( self ):
-        return self.data.keys()
-
-    def __len__( self ):
-        return len( self.data )
-
-    def get(self, key, default):
-        return self.data.get(key, default)
-
-    #______________________________________________________________________________
-
-    def error( self, txt1, txt2="" ):
-        print "Content-type: text/html\n"
-        print "<h1>Error: %s</h1>" % txt1
-        print txt2
-        import sys
-        sys.exit()
-
-    def debug( self ):
+    def debug(self):
         #~ print "Content-type: text/html\n"
         #~ print "<pre>"
         #~ for k,v in self.data.iteritems():
-            #~ self.page_msg( "%s - %s" % (k,v) )
-        #~ print cgi.FieldStorage( keep_blank_values=True )
+            #~ self.page_msg("%s - %s" % (k,v))
+        #~ print cgi.FieldStorage(keep_blank_values=True)
         #~ print "REQUEST_URI:",os.environ["REQUEST_URI"]
         #~ print "</pre>"
         import cgi
 
         import inspect
         # PyLucid's page_msg nutzen
-        self.page_msg( "-"*30 )
+        self.page_msg("-"*30)
         self.page_msg(
             "CGIdata Debug (from '...%s' line %s):" % (inspect.stack()[1][1][-20:], inspect.stack()[1][2])
         )
 
-        self.page_msg( "-"*30 )
+        self.page_msg("-"*30)
         for k,v in self.data.iteritems():
-            self.page_msg( "%s - %s" % ( k, cgi.escape(str(v)) ) )
-        self.page_msg( "FieldStorage:", cgi.FieldStorage( keep_blank_values=True ) )
+            self.page_msg("%s - %s" % (k, cgi.escape(str(v))))
+        self.page_msg("FieldStorage:", cgi.FieldStorage(keep_blank_values=True))
         try:
-            self.page_msg( 'os.environ["QUERY_STRING"]:', os.environ['QUERY_STRING'] )
+            self.page_msg('os.environ["QUERY_STRING"]:', os.environ['QUERY_STRING'])
         except:
             pass
         try:
-            self.page_msg( 'os.environ["REQUEST_URI"]:', os.environ["REQUEST_URI"] )
+            self.page_msg('os.environ["REQUEST_URI"]:', os.environ["REQUEST_URI"])
         except:
             pass
         try:
-            self.page_msg( 'os.environ["CONTENT_LENGTH"]:', os.environ["CONTENT_LENGTH"] )
+            self.page_msg('os.environ["CONTENT_LENGTH"]:', os.environ["CONTENT_LENGTH"])
         except:
             pass
-        self.page_msg( "-"*30 )
+        self.page_msg("-"*30)
 
 
 
@@ -221,30 +181,29 @@ class page_msg:
     """
     Kleine Klasse um die Seiten-Nachrichten zu verwalten
     page_msg wird in index.py den PyLucid-Objekten hinzugefugt.
-    mit PyLucid["page_msg"]( "Eine neue Nachrichtenzeile" ) wird Zeile
+    mit PyLucid["page_msg"]("Eine neue Nachrichtenzeile") wird Zeile
     für Zeile Nachrichten eingefügt.
     Die Nachrichten werden ganz zum Schluß in der index.py in die
     generierten Seite eingeblendet. Dazu dient der Tag <lucidTag:page_msg/>
     """
-    def __init__( self ):
+    def __init__(self):
         if config.system.page_msg_debug:
             self.data = "<p>[config.system.page_msg_debug = True!]</p>"
         else:
             self.data = ""
 
-    def __call__( self, *msg ):
+    def __call__(self, *msg):
         """ Fügt eine neue Zeile mit einer Nachricht hinzu """
         if config.system.page_msg_debug:
             # Angaben zur Datei, Zeilennummer, aus dem die Nachricht stammt
             filename = inspect.stack()[1][1].split("/")[-1][-20:]
-            fileinfo = "%-20s line %3s: " % (filename, inspect.stack()[1][2] )
+            fileinfo = "%-20s line %3s: " % (filename, inspect.stack()[1][2])
             self.data += fileinfo.replace(" ","&nbsp;")
 
-        self.data += "%s <br />\n" % " ".join( [str(i) for i in msg] )
+        self.data += "%s <br />\n" % " ".join([str(i) for i in msg])
 
-    def write( self, *msg ):
-        self.__call__( *msg )
-
+    def write(self, *msg):
+        self.__call__(*msg)
 
     def get(self):
         if self.data != "":
