191243a53cec29f1ca3f3ec93ad06e548df1ca16
jensdiemer <jensdiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2006-12-19 11:46:41 +0000

    sets backport

--- PyLucid/python_backports/backports.py
+++ PyLucid/python_backports/backports.py
@@ -4,13 +4,31 @@
 """
 Backport for some Python v2.4 features
 
-Some things taken from:
-http://aima.cs.berkeley.edu/python/utils.html
+Last commit info:
+----------------------------------
+$LastChangedDate:$
+$Rev:$
+$Author$
+
+Created by Jens Diemer
+
+license:
+    GNU General Public License v2 or above
+    http://www.opensource.org/licenses/gpl-license.php
+
 """
+
+__version__= "$Rev:$"
 
 
 import sys, __builtin__
 
 # Damit subprocess.py usw. gefunden werden:
 sys.path.insert(0,"PyLucid/python_backports")
+
+if not "set" in dir(__builtin__):
+    # set und frozenset - nutz das original sets.py aus Python 2.4
+    from sets import Set, ImmutableSet
+    __builtin__.set = Set
+    __builtin__.frozenset = ImmutableSet
 
