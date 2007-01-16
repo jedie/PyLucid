0f97dc67da1fc139f3c1dd72fecab3cf9cbe0807
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2007-01-16 08:31:55 +0000

    show_internals

---
+++ PyLucid/buildin_plugins/show_internals/plugin_cfg_debug.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""Plugin config debug
+
+Last commit info:
+----------------------------------
+$LastChangedDate:$
+$Rev:$
+$Author: jensdiemer $
+
+Created by Jens Diemer
+
+license:
+    GNU General Public License v2 or above
+    http://www.opensource.org/licenses/gpl-license.php
+"""
+
+__version__= "$Rev:$"
+
