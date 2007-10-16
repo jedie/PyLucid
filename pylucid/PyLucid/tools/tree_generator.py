7b59a3fd78a170c093e709008f60e1035c3683ca
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2007-10-16 12:17:29 +0000

    -add a group function. -expand the module test

--- pylucid/PyLucid/tools/tree_generator.py
+++ pylucid/PyLucid/tools/tree_generator.py
@@ -23,6 +23,7 @@
 
 
 class MenuNode(object):
+    group_id=None
     def __init__(self, id, data={}, parent=None):
         self.id = id
         self.data = data
@@ -42,12 +43,34 @@ class MenuNode(object):
         activate self + all sub nodes + the parent node
         """
         self.visible = True
+        
         # Activate all subnodes:
         for subnode in self.subnodes:
             subnode.visible = True
+            
         # activate the parent node
         if self.parent is not None:
             self.parent.activate()
+    
+    def activate_expanded(self, group_key, group_value):
+        """
+        enlarged the visible group.
+        Same as 'activate', but makes only self node, sub nodes and parent node 
+        visible, if data[group_key] == group_value
+        """
+        def conditional_activate(node):
+            if node.data[group_key] == group_value:
+                node.visible = True
+            else:
+                node.visible = False
+        
+        conditional_activate(self)
+        for subnode in self.subnodes:
+            conditional_activate(subnode)
+            
+        if self.parent is not None:
+            conditional_activate(self.parent)
+        
 
     def _get_current_entry(self, level):
         current_entry = self.data.copy()
@@ -136,6 +159,37 @@ class TreeGenerator(object):
         self.nodes[id].activate()
 
     #___________________________________________________________________________
+    
+    def get_group(self, group_key, id):
+        """
+        activate a coherent group, which have the same value in the 'group_key'.
+        """
+        self.deactivate_all()
+        
+        node = self.nodes[id]
+        group_value = node.data[group_key]
+
+        # go to the first parent node how has a other group value:
+        while 1:
+            if node.parent== None:
+                break
+            node = node.parent
+            if node.data[group_key] != group_value:
+                break
+        #print "first node with other group value:\n\t", node.data
+        
+        # activate all subnodes:
+        for subnode in node.subnodes:
+            subnode.activate_expanded(group_key, group_value)
+#            if subnode.data[group_key] == group_value:
+#                print "activate:", subnode.data
+#                subnode.activate(activate_parent=False)
+            
+        # return the coherent group
+        return node.to_dict()["subitems"]
+        
+        
+    #___________________________________________________________________________
 
     def get_menu_tree(self, id=None):
         """
@@ -160,144 +214,228 @@ class TreeGenerator(object):
         return self.root.get_flat_list()[1:]
 
 
-def test_generator(tree, display_result):
+
+#_______________________________________________________________________________
+# MODULE TEST:
+
+
+
+def _test_generator(tree, display_result):
     #
     # Sitemap.
     #
-    def verbose(result, no):
-        if not display_result:
-            return
+    def print_data(txt, data):
         print '-' * 40
-        print "*** No. %s ***" % no
-        pprint(result)
+        print "***** %s *****" % txt
+        pprint(data)
+        
+    def check(result, must_be, no):
+        failed = result != must_be
+        if failed or display_result:
+            txt = "No. %s - result:" % no
+            print_data(txt, result)
+        if failed:
+            txt = "ERROR %s - must be:" % no
+            print_data(txt, must_be)
+            
 
+        
     result = tree.get_sitemap_tree()
-    must_be = [{'id': 1,
-          'level': 1,
-          'name': '1. AAA',
-          'parent': None,
-          'subitems': [{'id': 2,
-                        'level': 2,
-                        'name': '1.1. BBB',
-                        'parent': 1,
-                        'subitems': [{'id': 4,
-                                      'level': 3,
-                                      'name': '1.2.1. CCC',
-                                      'parent': 2},
-                                     {'id': 5,
-                                      'level': 3,
-                                      'name': '1.2.2. CCC',
-                                      'parent': 2}]},
-                       {'parent': 1, 'id': 3, 'name': '1.2. BBB', 'level': 2}]},
-         {'id': 6,
-          'level': 1,
-          'name': '2. AAA',
-          'parent': None,
-          'subitems': [{'parent': 6, 'id': 7, 'name': '2.1. BBB', 'level': 2}]}]
-    verbose(result, 1)
-    if result != must_be:
-        print '-' * 40
-        print "*** ERROR 1 ***"
-        pprint(result)
+    must_be = [{'group': 'one',
+      'id': 1,
+      'level': 1,
+      'name': '1. AAA',
+      'parent': None,
+      'subitems': [{'group': 'two',
+                    'id': 2,
+                    'level': 2,
+                    'name': '1.1. BBB',
+                    'parent': 1},
+                   {'group': 'two',
+                    'id': 3,
+                    'level': 2,
+                    'name': '1.2. BBB',
+                    'parent': 1,
+                    'subitems': [{'group': 'two',
+                                  'id': 4,
+                                  'level': 3,
+                                  'name': '1.2.1. CCC',
+                                  'parent': 3},
+                                 {'group': 'two',
+                                  'id': 5,
+                                  'level': 3,
+                                  'name': '1.2.2. CCC',
+                                  'parent': 3}]}]},
+     {'group': 'one',
+      'id': 6,
+      'level': 1,
+      'name': '2. DDD',
+      'parent': None,
+      'subitems': [{'group': 'two',
+                    'id': 7,
+                    'level': 2,
+                    'name': '2.1. EEE',
+                    'parent': 6},
+                   {'group': 'two',
+                    'id': 8,
+                    'level': 2,
+                    'name': '2.2. EEE',
+                    'parent': 6}]}]
+    check(result, must_be, no=1)
 
 
     #
-    # Keinen Punkt aktivieren
-    #   => Nur die Punkte der ersten Ebene sind zu sehen.
+    # No menu point activated
+    #   => Display the first Level.
     #
     result = tree.get_menu_tree()
-    must_be = [{'parent': None, 'id': 1, 'name': '1. AAA', 'level': 1},
-            {'parent': None, 'id': 6, 'name': '2. AAA', 'level': 1}]
-    verbose(result, 2)
-    if result != must_be:
-        print '-' * 40
-        print "*** ERROR 2 ***"
-        pprint(result)
+    must_be = [
+        {'group': 'one', 'id': 1, 'level': 1, 'name': '1. AAA', 'parent': None},
+        {'group': 'one', 'id': 6, 'level': 1, 'name': '2. DDD', 'parent': None}
+    ]
+    check(result, must_be, no=2)
 
 
     #
-    # Menüpunkt 2 aktivieren.
+    # Activate the second menu point.
     #
     result = tree.get_menu_tree(2)
-    must_be = [{'id': 1,
-          'level': 1,
-          'name': '1. AAA',
-          'parent': None,
-          'subitems': [{'id': 2,
-                        'level': 2,
-                        'name': '1.1. BBB',
-                        'parent': 1,
-                        'subitems': [{'id': 4,
-                                      'level': 3,
-                                      'name': '1.2.1. CCC',
-                                      'parent': 2},
-                                     {'id': 5,
-                                      'level': 3,
-                                      'name': '1.2.2. CCC',
-                                      'parent': 2}]},
-                       {'parent': 1, 'id': 3, 'name': '1.2. BBB', 'level': 2}]},
-         {'parent': None, 'id': 6, 'name': '2. AAA', 'level': 1}]
-    verbose(result, 3)
-    if result != must_be:
-        print '-' * 40
-        print "*** ERROR 3 ***"
-        pprint(result)
+    must_be = [{'group': 'one',
+      'id': 1,
+      'level': 1,
+      'name': '1. AAA',
+      'parent': None,
+      'subitems': [{'group': 'two',
+                    'id': 2,
+                    'level': 2,
+                    'name': '1.1. BBB',
+                    'parent': 1},
+                   {'group': 'two',
+                    'id': 3,
+                    'level': 2,
+                    'name': '1.2. BBB',
+                    'parent': 1}]},
+     {'group': 'one', 'id': 6, 'level': 1, 'name': '2. DDD', 'parent': None}]
+    check(result, must_be, no=3)
 
 
     #
-    # Menüpunkt 6 aktivieren.
+    # Activate the sixth menu point.
     #
     result = tree.get_menu_tree(6)
-    must_be = [{'parent': None, 'id': 1, 'name': '1. AAA', 'level': 1},
-         {'id': 6,
+    must_be = [
+        {'group': 'one', 'id': 1, 'level': 1, 'name': '1. AAA', 'parent': None},
+         {'group': 'one',
+          'id': 6,
           'level': 1,
-          'name': '2. AAA',
+          'name': '2. DDD',
           'parent': None,
-          'subitems': [{'parent': 6, 'id': 7, 'name': '2.1. BBB', 'level': 2}]}]
-    verbose(result, 4)
-    if result != must_be:
-        print '-' * 40
-        print "*** ERROR 4 ***"
-        pprint(result)
+          'subitems': [{'group': 'two',
+                        'id': 7,
+                        'level': 2,
+                        'name': '2.1. EEE',
+                        'parent': 6},
+                       {'group': 'two',
+                        'id': 8,
+                        'level': 2,
+                        'name': '2.2. EEE',
+                        'parent': 6}]}]
+    check(result, must_be, no=4)
 
 
+    #
+    # a flat list for all current activated pages
+    #
+    result = tree.get_flat_list()
+    must_be = [
+        {'group': 'one', 'id': 1, 'level': 1, 'name': '1. AAA', 'parent': None},
+        {'group': 'one', 'id': 6, 'level': 1, 'name': '2. DDD', 'parent': None},
+        {'group': 'two', 'id': 7, 'level': 2, 'name': '2.1. EEE', 'parent': 6},
+        {'group': 'two', 'id': 8, 'level': 2, 'name': '2.2. EEE', 'parent': 6}
+    ]
+    check(result, must_be, no=5)
+
+
+    #
+    # generate a flat list from all pages
+    #
     tree.activate_all()
     result = tree.get_flat_list()
     must_be = [
-        {'id': 1, 'level': 1, 'name': '1. AAA', 'parent': None},
-        {'id': 2, 'level': 2, 'name': '1.1. BBB', 'parent': 1},
-        {'id': 4, 'level': 3, 'name': '1.2.1. CCC', 'parent': 2},
-        {'id': 5, 'level': 3, 'name': '1.2.2. CCC', 'parent': 2},
-        {'id': 3, 'level': 2, 'name': '1.2. BBB', 'parent': 1},
-        {'id': 6, 'level': 1, 'name': '2. AAA', 'parent': None},
-        {'id': 7, 'level': 2, 'name': '2.1. BBB', 'parent': 6}
+        {'group': 'one', 'id': 1, 'level': 1, 'name': '1. AAA', 'parent': None},
+        {'group': 'two', 'id': 2, 'level': 2, 'name': '1.1. BBB', 'parent': 1},
+        {'group': 'two', 'id': 3, 'level': 2, 'name': '1.2. BBB', 'parent': 1},
+        {'group': 'two', 'id': 4, 'level': 3, 'name': '1.2.1. CCC', 'parent': 3},
+        {'group': 'two', 'id': 5, 'level': 3, 'name': '1.2.2. CCC', 'parent': 3},
+        {'group': 'one', 'id': 6, 'level': 1, 'name': '2. DDD', 'parent': None},
+        {'group': 'two', 'id': 7, 'level': 2, 'name': '2.1. EEE', 'parent': 6},
+        {'group': 'two', 'id': 8, 'level': 2, 'name': '2.2. EEE', 'parent': 6}
     ]
-    verbose(result, 5)
-    if result != must_be:
-        print '-' * 40
-        print "*** ERROR 5 ***"
-        pprint(result)
+    check(result, must_be, no=6)
+    
+    
+    #
+    # a coherent group, which have the same value in the 'group_key'
+    #
+    result = tree.get_group(group_key="group", id=5)
+    must_be = [
+     {'group': 'two', 'id': 2, 'level': 1, 'name': '1.1. BBB', 'parent': 1},
+     {'group': 'two',
+      'id': 3,
+      'level': 1,
+      'name': '1.2. BBB',
+      'parent': 1,
+      'subitems': [{'group': 'two',
+                    'id': 4,
+                    'level': 2,
+                    'name': '1.2.1. CCC',
+                    'parent': 3},
+                   {'group': 'two',
+                    'id': 5,
+                    'level': 2,
+                    'name': '1.2.2. CCC',
+                    'parent': 3}]}
+    ]
+    check(result, must_be, no=7)
+    
+    result = tree.get_group(group_key="group", id=8)
+    must_be = [
+        {'group': 'two', 'id': 7, 'level': 1, 'name': '2.1. EEE', 'parent': 6},
+        {'group': 'two', 'id': 8, 'level': 1, 'name': '2.2. EEE', 'parent': 6}
+    ]
+    check(result, must_be, no=8)
 
+    result = tree.get_group(group_key="parent", id=3)
+    must_be = [
+         {'group': 'two', 'id': 2, 'level': 1, 'name': '1.1. BBB', 'parent': 1},
+         {'group': 'two', 'id': 3, 'level': 1, 'name': '1.2. BBB', 'parent': 1},
+    ]
+    check(result, must_be, no=9)
+    
+
+#_______________________________________________________________________________
 
 
 if __name__ == "__main__":
+    print "module test - START\n"
     from pprint import pprint
 
     data = [
-        {'id': 1, 'parent': None, 'name': '1. AAA'},
-        {'id': 2, 'parent': 1,    'name': '1.1. BBB'},
-        {'id': 3, 'parent': 1,    'name': '1.2. BBB'},
-        {'id': 4, 'parent': 2,    'name': '1.2.1. CCC'},
-        {'id': 5, 'parent': 2,    'name': '1.2.2. CCC'},
-        {'id': 6, 'parent': None, 'name': '2. AAA'},
-        {'id': 7, 'parent': 6,    'name': '2.1. BBB'},
+        {'id': 1, 'parent': None, 'group': 'one', 'name': '1. AAA'},
+        {'id': 2, 'parent': 1,    'group': 'two', 'name': '1.1. BBB'},
+        {'id': 3, 'parent': 1,    'group': 'two', 'name': '1.2. BBB'},
+        {'id': 4, 'parent': 3,    'group': 'two', 'name': '1.2.1. CCC'},
+        {'id': 5, 'parent': 3,    'group': 'two', 'name': '1.2.2. CCC'},
+        {'id': 6, 'parent': None, 'group': 'one', 'name': '2. DDD'},
+        {'id': 7, 'parent': 6,    'group': 'two', 'name': '2.1. EEE'},
+        {'id': 8, 'parent': 6,    'group': 'two', 'name': '2.2. EEE'},
     ]
-
-    print "test..."
+    print "Source data:"
+    pprint(data)
 
     tree = TreeGenerator(data)
-#    test_generator(tree, display_result=False)
-    test_generator(tree, display_result=True)
+    _test_generator(tree, display_result=False)
+#    _test_generator(tree, display_result=True)
 
-    print "\nEND"
+    print "\nmodule test - END"
 
