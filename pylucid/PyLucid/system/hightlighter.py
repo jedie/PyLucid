0d69ec6a7260e632a5f9264cac0f60cf8afde907
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2007-10-31 15:52:25 +0000

    use lexers.guess_lexer()

--- pylucid/PyLucid/system/hightlighter.py
+++ pylucid/PyLucid/system/hightlighter.py
@@ -1,10 +1,30 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+"""
+    PyLucid <-> Pygments
+    ~~~~~~~~~~~~~~~~~~~~
+
+    hightlight sourcecode
+
+    http://pygments.org/
+
+    Last commit info:
+    ~~~~~~~~~~~~~~~~~
+    $LastChangedDate: $
+    $Rev: $
+    $Author: $
+
+    :copyright: 2007 by Jens Diemer
+    :license: GNU GPL v3, see LICENSE.txt for more details.
+"""
 
 from PyLucid.tools.content_processors import escape, escape_django_tags
 
+from django.utils.translation import ugettext as _
 from django.conf import settings
 
 from PyLucid.system.response import SimpleStringIO
-
 
 try:
     from pygments import lexers
@@ -31,6 +51,12 @@ def no_hightlight(code):
     html = '\n<pre><code>%s</code></pre>\n' % escape(code)
     return html
 
+def get_formatter():
+    formatter = HtmlFormatter(
+        linenos=True, encoding="utf-8", style='colorful',
+        cssclass = CSSCLASS,
+    )
+    return formatter
 
 def pygmentize(sourcecode, source_type):
     """
@@ -41,29 +67,22 @@ def pygmentize(sourcecode, source_type):
         html = no_hightlight(sourcecode)
         return html, lexer_name
 
-    ext = source_type.lower().lstrip(".")
+    ext = source_type.lower().lstrip(".").strip("'\"")
 
     try:
-        lexer = lexers.get_lexer_by_name(ext)
+        if ext == "":
+            lexer = lexers.guess_lexer(sourcecode)
+        else:
+            lexer = lexers.get_lexer_by_name(ext)
     except lexers.ClassNotFound, err:
-        lexer_name = "[Hightlight Error: %s]<br />\n" % err
+        info = _("unknown type")
+        lexer_name = '<small title="%s">%s</small>' % (err, info)
         html = no_hightlight(sourcecode)
         return html, lexer_name
 
-
     lexer_name = lexer.name
 
-    formatter = HtmlFormatter(
-        linenos=True, encoding="utf-8", style='colorful',
-        cssclass = CSSCLASS,
-    )
-
-    #stylesheet = formatter.get_style_defs(CSSCLASS)
-
-#    self.context["css_data"].append({
-#        "from_info": "tinyTextile",
-#        "data": stylesheet,
-#    })
+    formatter = get_formatter()
 
     out_object = SimpleStringIO()
     highlight(sourcecode, lexer, formatter, out_object)
@@ -73,4 +92,5 @@ def pygmentize(sourcecode, source_type):
     html = escape_django_tags(html)
 
     return html, lexer_name
+
 
