594cdd63daa86746976c0f53d3bfb1500566f380
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-01-03 08:14:07 +0000

    use the environment variable DJANGO_SETTINGS_MODULE instead of "--settings="

--- pylucid/django-admin.sh
+++ pylucid/django-admin.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-#export DJANGO_SETTINGS_MODULE=PyLucid.settings
+#
+# This shell script is usefull, if you want to use django-admin with the
+# local django packages and not with a normal installed django instance.
+#
+
+export DJANGO_SETTINGS_MODULE=PyLucid.settings
 
 # use the local django packages
 export PYTHONPATH=${PWD}
 
-python ./django/bin/django-admin.py --settings=PyLucid.settings $*
+python ./django/bin/django-admin.py $*
