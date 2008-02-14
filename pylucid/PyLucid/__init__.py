bd1f1d8d833d87e08fd27e7bf9c2277f8c2a8bd2
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-02-14 15:23:36 +0000

    work-a-round using /setup.py in the lite version without django in the sys.path

--- pylucid/PyLucid/__init__.py
+++ pylucid/PyLucid/__init__.py
@@ -16,15 +16,19 @@
     :license: GNU GPL v3, see LICENSE.txt for more details.
 """
 
-from django.utils.version import get_svn_revision
+try:
+    from django.utils.version import get_svn_revision
 
-svn_revision = get_svn_revision("PyLucid")
-if svn_revision == u'SVN-unknown':
-    # No SVN checkout, a release?
+    svn_revision = get_svn_revision("PyLucid")
+    if svn_revision == u'SVN-unknown':
+        # No SVN checkout, a release?
+        svn_revision = ""
+    else:
+        # Add a space between the normal version number and the SVN revision number.
+        svn_revision = " " + svn_revision
+except ImportError:
+    # using /setup.py in the lite version without django in the sys.path
     svn_revision = ""
-else:
-    # Add a space between the normal version number and the SVN revision number.
-    svn_revision = " " + svn_revision
 
 # PyLucid Version String
 # Important for setuptools:
