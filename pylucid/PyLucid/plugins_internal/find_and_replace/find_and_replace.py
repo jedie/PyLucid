1b8edaffea3776fcd0186427934d2e358a258175
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-02-18 16:41:27 +0000

    add the feature for find/replace operation into template and stylesheet content ;)

--- pylucid/PyLucid/plugins_internal/find_and_replace/find_and_replace.py
+++ pylucid/PyLucid/plugins_internal/find_and_replace/find_and_replace.py
@@ -5,7 +5,13 @@
     PyLucid find and replace
     ~~~~~~~~~~~~~~~~~~~~~~~~
 
-    Find/Replace in all CMS page content.
+    Find/Replace in all page/template/stylesheet content.
+
+    Known bugs:
+        SQLite doesn’t support case-sensitive LIKE statements, we add some
+        "SQLite work-a-round" for this. Otherwise we would get a empty result
+        list. Note: The real replace operation always works case-sensitive.
+        See also: http://www.djangoproject.com/documentation/db-api/#contains
 
     Last commit info:
     ~~~~~~~~~
@@ -25,7 +31,7 @@ from django import newforms as forms
 from django.utils.translation import ugettext as _
 from django.utils.safestring import mark_safe
 
-from PyLucid.models import Page
+from PyLucid.models import Page, Template, Style
 from PyLucid.system.BasePlugin import PyLucidBasePlugin
 from PyLucid.tools.utils import escape
 from PyLucid.tools.Diff import diff_lines
@@ -35,6 +41,13 @@ MIN_TERM_LEN = 2
 MAX_TERM_LEN = 150
 
 
+# for FindReplaceForm ChoiceField
+TYPES = (
+    ("pages", "pages"),
+    ("templates", "templates"),
+    ("stylesheets", "stylesheets"),
+)
+
 class FindReplaceForm(forms.Form):
     # TODO: min und max should be saved in the prefereces.
     find_string = forms.CharField(
@@ -42,6 +55,10 @@ class FindReplaceForm(forms.Form):
     )
     replace_string = forms.CharField(
         min_length = MIN_TERM_LEN, max_length = MAX_TERM_LEN,
+    )
+    type = forms.ChoiceField(
+        choices=TYPES,
+        help_text = _("Please select the content type for the operation.")
     )
     simulate = forms.BooleanField(
         initial = True, help_text = _("Don't replace anything.")
@@ -66,33 +83,56 @@ class find_and_replace(PyLucidBasePlugin):
             form = FindReplaceForm()
 
         context["form"] = form
-        self._render_template("find_and_replace", context)
+        self._render_template("find_and_replace", context)#, debug=True)
 
 
-    def do(self, find_string, replace_string, simulate):
+    def do(self, find_string, replace_string, type, simulate):
         """
         Do the find/replace action.
         Returns a result list with diff information.
         """
-        pages = Page.objects.all().filter(content__icontains=find_string)
-        if len(pages) == 0:
-            self.page_msg("No pages contains the string '%s'" % find_string)
+        def nothing_found():
+            # We used this two times
+            self.page_msg(
+                "No %s contains the string '%s'" % (type, find_string)
+            )
             return None, None
+
+        if type == "pages":
+            model_object = Page
+        elif type == "templates":
+            model_object = Template
+        elif type == "stylesheets":
+            model_object = Style
+        else:
+            self.page_msg.red("Wrong type!")
+            return None, None
+
+        items = model_object.objects.all().filter(
+            content__contains=find_string
+        )
+        if len(items) == 0:
+            return nothing_found()
 
         total_changes = 0
         results = []
-        changed_pages = []
-        for page in pages:
-            old_content = page.content
+        changed_items = []
+        for item in items:
+            old_content = item.content
 
             changes = old_content.count(find_string)
+            if changes == 0:
+                # SQlite work-a-round for the
+                continue
+
             total_changes += changes
 
             new_content = old_content.replace(find_string, replace_string)
             if not simulate:
-                page.content = new_content
-                page.save()
-                changed_pages.append(page.name)
+                # Save the find/replace result
+                item.content = new_content
+                item.save()
+                changed_objects.append(item.name)
 
             diff = diff_lines(old_content, new_content)
             diff = escape(diff)
@@ -100,16 +140,19 @@ class find_and_replace(PyLucidBasePlugin):
 #            self.page_msg(diff)
 
             results.append({
-                "page": page,
+                "item": item,
                 "changes": changes,
                 "diff": diff,
             })
 
-        if not simulate:
-            self.page_msg.green("Changed pages:")
-            self.page_msg.green(", ".join(changed_pages))
+        if total_changes == 0:
+            # SQLite work-a-round
+            return nothing_found()
 
-#        self.page_msg(results)
+        if not simulate:
+            self.page_msg.green("Changed %s:" % type)
+            self.page_msg.green(", ".join(changed_items))
+
         return results, total_changes
 
 
--- pylucid/PyLucid/plugins_internal/find_and_replace/find_and_replace_cfg.py
+++ pylucid/PyLucid/plugins_internal/find_and_replace/find_and_replace_cfg.py
@@ -6,10 +6,12 @@
 
 __author__      = "Jens Diemer"
 __url__         = "http://www.PyLucid.org"
-__description__ = "Find & replace content in all CMS pages."
+__description__ = "Find & replace content in all pages/templates/shylesheets."
 __long_description__ = """
-A simple find & replace plugin. You can easy change e.g. a URL in every page
-content or correct a misspelled word everywhere :)
+A simple find & replace plugin for all pages/templates/shylesheets content.
+You can easy change e.g. a URL in every page content or correct a misspelled
+word everywhere. Also useable to change CSS colors everywhere.
+
 Note: This plugin can only administrator used! You find the plugin in the
 submenu.
 """
