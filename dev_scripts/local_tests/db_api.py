05f32aa0fce8415afb164d2bbd76f746ad7f6c6c
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-03-06 13:29:09 +0000

    local_tests: Use the old setup routines

--- dev_scripts/local_tests/db_api.py
+++ dev_scripts/local_tests/db_api.py
@@ -33,3 +33,16 @@ shortcuts = Page.objects.values("shortcut").exclude(shortcut=shortcut)
 
 shortcuts = [i["shortcut"] for i in shortcuts]
 print shortcuts
+
+print "-----------------------------------------------------------------------"
+
+page = Page.objects.all()[0]
+
+print "Build a list of all model field names:"
+fields = []
+for field in page._meta.fields:
+#    print field
+    field_name = field.column.replace('_id','')
+    fields.append(field_name)
+
+print fields
--- dev_scripts/local_tests/setup.py
+++ dev_scripts/local_tests/setup.py
@@ -5,15 +5,91 @@
 Setup the environment for the "local_dev_tests".
 """
 
-# Go into the root folder and insert it into the sys.path
-import os, sys
-#print "os.getcwd() 1:", os.getcwd()
-os.chdir("../unittests")
-#print "os.getcwd() 2:", os.getcwd()
-sys.path.insert(0, os.getcwd())
+import sys, os
+from StringIO import StringIO
 
-# make setup available
-from setup_environment import setup
+def setup_path(chdir, path_info):
+    if path_info:
+        print "os.getcwd() 1:", os.getcwd()
+    if chdir == None:
+        # Nothing to do (e.g. for local_dev_tests)
+        return
+    os.chdir(chdir) # go into PyLucid App root folder
+    if path_info:
+        print "os.getcwd() 2:", os.getcwd()
+    sys.path.insert(0, os.getcwd())
+
+def setup_django_environ():
+    os.environ["DJANGO_SETTINGS_MODULE"] = "PyLucid.settings"
+    from PyLucid import settings
+
+    settings.DATABASE_ENGINE = "sqlite3"
+    settings.DATABASE_NAME = ":memory:"
+
+    print "- setup the django environ...",
+    from django.core import management
+    management.setup_environ(settings) # init django
+    from django.test.utils import setup_test_environment
+    setup_test_environment() # django global pre-test setup
+    print "OK"
+
+def make_syncdb():
+    print "- create the model tables...",
+    from django.core import management
+    management.call_command('syncdb', verbosity=0, interactive=False)
+    print "OK"
+
+def make_insert_dump(extra_verbose):
+    print "- insert the PyLucid install Dump...",
+    if extra_verbose:
+        print
+    from PyLucid.install.install import DB_DumpFakeOptions
+    from PyLucid.tools.db_dump import loaddb
+
+    fake_options = DB_DumpFakeOptions()
+    fake_options.verbose = extra_verbose
+
+    if not extra_verbose:
+        old_stderr = sys.stderr
+        sys.stderr = StringIO()
+    try:
+        loaddb(app_labels = [], format = "py", options = fake_options)
+    finally:
+        if not extra_verbose:
+            sys.stderr = old_stderr
+
+    print "OK"
+
+def install_internal_plugins(extra_verbose):
+    print "- install internal plugins...",
+    if extra_verbose:
+        print "extra verbose is ON."
+
+    from PyLucid.system.plugin_manager import auto_install_plugins
+
+    auto_install_plugins(request = None, extra_verbose=extra_verbose)
+    print "OK"
+
+
+def setup(chdir="../../pylucid", path_info=True, extra_verbose=True,
+                        syncdb=True, insert_dump=True, install_plugins=False):
+    """
+    setup a test environment
+    """
+    print "Setup a local django environment for testing:"
+    setup_path(chdir, path_info)
+    setup_django_environ()
+    if syncdb:
+        make_syncdb()
+
+    if insert_dump:
+        make_insert_dump(extra_verbose)
+
+    if install_plugins:
+        install_internal_plugins(extra_verbose)
+
+    print "-"*80
+    print
 
 if __name__ == "__main__":
     print "Local Test:"
