a4fcca4af76a013713b95c55ba7bae26a32e498e
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-03-10 07:49:27 +0000

    unittest: Try to access restricted _command methods as a not active user.

--- pylucid/tests/test_plugin_manager.py
+++ pylucid/tests/test_plugin_manager.py
@@ -20,8 +20,11 @@
 """
 
 import tests
+from tests import TEST_USERS, TEST_UNUSABLE_USER
 
 from django.conf import settings
+
+from PyLucid.models import User
 
 # Open only one traceback in a browser (=True) ?
 #ONE_BROWSER_TRACEBACK = False
@@ -44,19 +47,34 @@ class TestBase(tests.TestCase):
         settings.DEBUG=False
 
         self.base_url = "/%s/1" % settings.COMMAND_URL_PREFIX
-
-
-class AnonymousTest(TestBase):
-    """
-    Test as a anonymous user
-    """
-    def test_access_deny(self):
+        
+    def _access_deny_test(self):
         plugin_name = "page_admin"
         method_names = (
             "edit_page", "new_page", "select_edit_page",
             "delete_pages", "sequencing"
         )
         self.assertAccessDenied(self.base_url, plugin_name, method_names)
+
+
+class AnonymousTest(TestBase):
+    """
+    Try to access restricted _command methods as a anonymous user.
+    """
+    def test_access_deny(self):
+        self._access_deny_test()
+
+
+class DeactivatedUserTest(TestBase):
+    """
+    Try to access restricted _command methods as a not active user.
+    """
+    def test_access_deny(self):
+        # Deactivate all users
+        for user in User.objects.all():
+            user.is_active = False
+            
+        self._access_deny_test()
 
 
 class NotAdminTest(TestBase):
