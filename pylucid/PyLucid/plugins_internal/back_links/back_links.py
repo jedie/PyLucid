21731d0d8263024394e172b07c552eeeb74c03f4
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-05-23 16:34:29 +0000

    update back_links plugin. Use plugin preferences.

--- pylucid/PyLucid/plugins_internal/back_links/back_links.py
+++ pylucid/PyLucid/plugins_internal/back_links/back_links.py
@@ -34,17 +34,39 @@
 __version__= "$Rev$"
 
 
+from django.utils.safestring import mark_safe
+
 from PyLucid.system.BasePlugin import PyLucidBasePlugin
-from PyLucid.models import Page
+from PyLucid.models import Page, Plugin
 
 class back_links(PyLucidBasePlugin):
 
-    def lucidTag(self, print_last_page=False, print_index=False, index="Index"):
+    def lucidTag(self, **kwargs):
         """
         generate the backlinks
+        TODO: **kwargs only for Backwards-incompatible changes info
         """
+        if self.request.debug == True and kwargs != {}:
+            self.page_msg(
+                "DeprecationWarning:"
+                " kwargs in back_links plugin are obsolete. Please remove them."
+            )
+            return
+
+        # Get the preferences from the database:
+        preferences = self.get_preferences()
+        if preferences == None:
+            # preferences not in database -> reinit required
+            if self.request.debug == True:
+                msg = (
+                    '<a href="http://www.pylucid.org/_goto/121/changes/#23-05-2008-back_links">'
+                    'reinit "back_links" plugin required!</a>'
+                )
+                self.page_msg.red(mark_safe(msg))
+                return
+
         current_page = self.context["PAGE"]
-        if print_last_page == True:
+        if preferences["print_last_page"] == True:
             print_page = current_page
         else:
             print_page = current_page.parent
@@ -52,17 +74,17 @@ class back_links(PyLucidBasePlugin):
                 # There exist no higher-ranking page
                 return ""
 
-        data = self.backlink_data(print_page)
+        data = self._backlink_data(print_page)
         data.reverse()
 
         context = {
             "pages": data,
-            "print_index": print_index,
-            "index": index,
+            "print_index": preferences["print_index"],
+            "index": preferences["index"],
         }
         self._render_template("back_links", context)#, debug=True)
 
-    def backlink_data(self, parent_page):
+    def _backlink_data(self, parent_page):
         """
         make a list of all pages in the current way back to the index page.
         """
--- pylucid/PyLucid/plugins_internal/back_links/back_links_cfg.py
+++ pylucid/PyLucid/plugins_internal/back_links/back_links_cfg.py
@@ -11,6 +11,29 @@ Puts links to every lower level page into the CMS page.
 """
 
 #_____________________________________________________________________________
+# preferences
+
+from django import newforms as forms
+from django.utils.translation import ugettext as _
+
+class PreferencesForm(forms.Form):
+    print_last_page = forms.BooleanField(
+        initial = True,
+        help_text = _(
+            "If checked the actual page will be the last page in the bar."
+            " Otherwise the parentpage."
+        ),
+    )
+    print_index = forms.BooleanField(
+        initial = True,
+        help_text = _('If checked display a link to the index ("/")'),
+    )
+    index = forms.CharField(
+        initial = _("Index"),
+        help_text = _('the name that is printed for the indexpage'),
+    )
+
+#_____________________________________________________________________________
 # plugin administration data
 
 plugin_manager_data = {
