bf677bec8cdc2508e709c26b8a404bb27eb11408
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-09-10 08:23:24 +0000

    change Version String to v0.8.5

--- pylucid_project/PyLucid/__init__.py
+++ pylucid_project/PyLucid/__init__.py
@@ -36,7 +36,7 @@ except ImportError:
 # - No spaces: "0.8.0 RC2" -> "0.8.0RC2"
 # http://peak.telecommunity.com/DevCenter/setuptools#specifying-your-project-s-version
 PYLUCID_VERSION = (0, 8, 5, svn_revision)
-PYLUCID_VERSION_STRING = "0.8.5RC1" + svn_revision
+PYLUCID_VERSION_STRING = "0.8.5" + svn_revision
 
 
 #______________________________________________________________________________
