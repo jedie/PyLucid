505902009354dbd80361d5482afc3854ff14489f
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2008-10-23 07:37:49 +0000

    check if settings.py exist in win start batch.

--- pylucid_project/standalone_w32.cmd
+++ pylucid_project/standalone_w32.cmd
@@ -3,6 +3,18 @@
 REM go into the same directory where this file stored (Project root)
 cd /d "%~dp0"
 
+if not exist "%~dp0PyLucid\settings.py" (
+    echo.
+    echo Error: There exist no settings.py!
+    echo.
+    echo Please create "%~dp0PyLucid\settings.py"
+    echo.
+    echo More info at http://www.pylucid.org
+    echo.
+    pause
+    exit
+)
+
 REM use the local django packages
 set PYTHONPATH="%~dp0"
 
