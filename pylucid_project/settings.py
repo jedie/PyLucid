b107d46b25e2321d087131a9be98f8e12060f87c
JensDiemer <git@jensdiemer.de>
2009-03-30 16:22:12 +0000

    add svn:externals and add test settings.py + test.db3 SQLite file with old v0.8 dump data (User "test" + password "12345678")

--- pylucid_project/settings.py
+++ pylucid_project/settings.py
@@ -8,7 +8,7 @@ DEBUG = True
 
 # Database connection info.
 DATABASE_ENGINE = 'sqlite3'    # 'postgresql', 'mysql', 'sqlite3' or 'ado_mssql'.
-DATABASE_NAME = 'test_pylucid_v0.8.db3'  # Or path to database file if using sqlite3.
+DATABASE_NAME = 'test.db3'     # Or path to database file if using sqlite3.
 DATABASE_USER = ''             # Not used with sqlite3.
 DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
@@ -20,7 +20,7 @@ ROOT_URLCONF = 'pylucid_project.urls'
 _BASE_PATH = os.path.join(os.path.dirname(__file__))
 TEMPLATE_DIRS = (
     os.path.join(_BASE_PATH, "apps/pylucid/templates/"),
-    
+
     os.path.join(_BASE_PATH, "django/contrib/admin/templates"),
 )
 
