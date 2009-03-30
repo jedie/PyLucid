144f3a85cc1a8ec2ae731126cc7263ae8cdbd3c2
JensDiemer <JensDiemer@d814239b-9e20-0410-bfe5-ce12f5c20d43>
2009-03-30 16:03:54 +0000

    make creole markup a little more robust.

--- pylucid_project/PyLucid/system/markups/PyLucid_creole_macros.py
+++ pylucid_project/PyLucid/system/markups/PyLucid_creole_macros.py
@@ -11,14 +11,16 @@ from PyLucid.system.markups.creole.default_macros import *
 
 def code(args, text):
     """
-    Highlight sourcecode using 
+    Highlight sourcecode using
     Macro tag <<code ext=.EXT>>...<</code>>
     """
     if "=" in args:
-        source_type = args.split("=")[-1]
+        source_type = args.split("=")[-1].strip("\" '")
+        if not source_type.startswith("."):
+            source_type = ".%s" % source_type
     else:
         source_type = ""
-        
+
     return make_html(
         sourcecode = text,
         source_type=source_type,
