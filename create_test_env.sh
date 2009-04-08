aaef532b7d733b16b7964b2fc3d1201a5e229c81
JensDiemer <git@jensdiemer.de>
2009-04-08 12:02:59 +0000

    first virtualenv + pip tests

--- create_test_env.sh
+++ create_test_env.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 function verbose_eval {
-    echo ---------------------------------------------------------------------
+    echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     echo $*
-    echo ---------------------------------------------------------------------
+    echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     eval $*
+    echo ---------------------------------------------------------------------
+    echo
 }
 
-verbose_eval svn checkout http://code.djangoproject.com/svn/django/trunk/django/
+PYLUCID_ENV=PyLucid_env
+BIN=${PYLUCID_ENV}/bin
+
+# This script created by scripts/create_bootstrap_script.py
+BOOTSTRAP_SCRIPT=scripts/pylucid-boot.py
+
+echo _____________________________________________________________________
+echo Create virtual environment:
+verbose_eval python ${BOOTSTRAP_SCRIPT} ${PYLUCID_ENV}
+
+echo _____________________________________________________________________
+echo activate the virtual environment:
+verbose_eval source ${BIN}/activate
+
+echo _____________________________________________________________________
+echo Install external dependencies using pip:
+verbose_eval ${BIN}/python ${BIN}/pip install --requirement requirements/external_apps.txt
