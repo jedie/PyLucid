c168f91395cb41d1e9dc7d833aadf5dd01ec24be
JensDiemer <git@jensdiemer.de>
2009-04-15 07:09:47 +0000

    bugfix: IMHO a slug can contain a "-" character

--- pylucid_project/apps/pylucid/urls.py
+++ pylucid_project/apps/pylucid/urls.py
@@ -21,5 +21,5 @@ from pylucid_project.apps.pylucid.views import root_page, lang_root_page, resolv
 urlpatterns = patterns('',
     url(r'^$', root_page, name='PyLucid-root_page'),
     url(r'^(?P<lang_code>[a-zA-Z-_]{2,}?)/$', lang_root_page, name='PyLucid-lang_root_page'),
-    url(r'^(?P<lang_code>[a-zA-Z-_]{2,}?)/(?P<url_path>[\w/]+?)/$', resolve_url, name='PyLucid-resolve_url'),
+    url(r'^(?P<lang_code>[a-zA-Z-_]{2,}?)/(?P<url_path>[\w/-]+?)/$', resolve_url, name='PyLucid-resolve_url'),
 )
