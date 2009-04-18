f76345bd15a9f66eef1fd964594e0f48217a927d
JensDiemer <git@jensdiemer.de>
2009-04-18 23:26:58 +0000

    quick update existing sitemap plugin, but only use flat list with page.get_absolute_url

---
+++ pylucid_project/pylucid_plugins/SiteMap/SiteMap_cfg.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+#_____________________________________________________________________________
+# meta information
+
+__author__      = "Jens Diemer"
+__url__         = "http://www.PyLucid.org"
+__description__ = "SiteMap generator"
+__long_description__ = """
+A StiteMap generator.
+"""
+
+#_____________________________________________________________________________
+# plugin administration data
+
+plugin_manager_data = {
+    "lucidTag" : {
+        "must_login"    : False,
+        "must_admin"    : False,
+    }
+}
+
---
+++ pylucid_project/pylucid_plugins/SiteMap/views.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+
+"""
+    PyLucid sitemap
+    ~~~~~~~~~~~~~~~
+
+    ToDo: Use the Template to generate the Sitemap tree.
+    But there is no recuse-Tag in the django template engine :(
+    - http://www.python-forum.de/topic-9655.html
+    - http://groups.google.com/group/django-users/browse_thread/thread/3bd2812a3d0f7700/14f61279e0e9fd90
+
+    Last commit info:
+    ~~~~~~~~~~~~~~~~~
+    $LastChangedDate$
+    $Rev$
+    $Author$
+
+    :copyleft: 2007 by the PyLucid team, see AUTHORS for more details.
+    :license: GNU GPL v2 or above, see LICENSE for more details
+"""
+
+__version__= "$Rev$"
+
+from django.utils.safestring import mark_safe
+
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+from pylucid.models import PageContent, PageTree
+
+#HTML_TEMPLATE = (
+#    '<li>'
+#    '    <p class="deep_%(level)s">'
+#    '    <a href="%(href)s" title="%(title)s">%(name)s</a> - %(title)s'
+#    '    </p>'
+#    '</li>'
+#)
+
+
+def lucidTag(request):
+    """
+    Create the sitemap tree
+    TODO: We need a real tree here!
+    """
+    
+    pages = PageContent.objects.all()
+    
+    context = {"pages": pages}
+
+    return render_to_response('SiteMap/SiteMap.html', context, 
+        context_instance=RequestContext(request)
+    )
+    
+    
+#
+#    # Get a tree dict of all pages:
+#    sitemap_tree = get_sitemap_tree(self.request)
+#
+#    # Generate the html page
+#    # TODO: This nomaly is the job from the django template engine :(
+#    html = self.get_html(sitemap_tree)
+#
+#    html = mark_safe(html) # turn djngo auto-escaping off
+#
+#    context = {
+#        "sitemap": html,
+#    }
+#
+#    # The CSS data should be added into the CMS page:
+#    self._render_template("SiteMap", context)
+#
+#
+#def get_html(menu_data, parent=None):
+#    """
+#    Generate the html code from the given tree data:
+#
+#    [{'id': 1L,
+#      'level': 1,
+#      'name': 'index',
+#      'parent': 0L,
+#      'shortcut': 'Index',
+#      'title': 'index'},
+#     {'id': 10L,
+#      'level': 1,
+#      'name': 'Designs',
+#      'parent': 0L,
+#      'shortcut': 'Designs',
+#      'subitems': [{'id': 13L,
+#                    'level': 2,
+#                    'name': 'elementary',
+#                    'parent': 10L,
+#                    'shortcut': 'Elementary',
+#                    'title': 'elementary'},
+#                   {'id': 12L,
+#                    'level': 2,
+#                    'name': 'old defaul
+#    """
+#    result = ["<ul>"]
+#
+#    for entry in menu_data:
+#        href = []
+#        if parent:
+#            href.append(parent)
+#
+#        href.append(entry["shortcut"])
+#        
+#        href = "/".join(href)
+#
+#        if entry["showlinks"] == True:
+#            entry["href"] = self.URLs["absoluteIndex"] + href + "/"
+#            result.append(HTML_TEMPLATE % entry)
+#
+#        if "subitems" in entry:
+#            result.append(
+#                self.get_html(entry["subitems"], parent=href)
+#            )
+#
+#    result.append("</ul>")
+#    return "\n".join(result)
+#
+#
+#
+#
+#
