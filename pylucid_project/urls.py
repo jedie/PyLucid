dc48dd38a324dc5a76e7f72d767448529a88bda7
JensDiemer <git@jensdiemer.de>
2009-06-04 10:11:02 +0000

    * add superfish menu into admin panel. * move update section to /admin/update/

--- pylucid_project/urls.py
+++ pylucid_project/urls.py
@@ -26,10 +26,12 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     #_____________________________________
+    # PYLUCID UPDATE SECTION
+    ('^%s/update/'% settings.ADMIN_URL_PREFIX, include('pylucid_update.urls')),
+    
+    #_____________________________________
     # DJANGO ADMIN PANEL
     (r'^%s/' % settings.ADMIN_URL_PREFIX, include(admin.site.urls)),
-
-    ('^update/', include('pylucid_update.urls')),
 )
 
 # serve static files
