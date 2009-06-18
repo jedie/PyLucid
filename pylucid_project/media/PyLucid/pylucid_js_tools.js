803ff3db0c2e13116fe8ff11ffcc9fd336aff68a
JensDiemer <git@jensdiemer.de>
2009-06-18 16:29:26 +0000

    work-a-round: We must find a way to handle redirects!

--- pylucid_project/media/PyLucid/pylucid_js_tools.js
+++ pylucid_project/media/PyLucid/pylucid_js_tools.js
@@ -15,6 +15,53 @@ function OpenInWindow(URL) {
 }
 
 
+function replace_complete_page(html) {
+	// replace the complete page
+	document.open() // no append available since it is closed
+	document.write(html);
+	document.close(); 
+}
+
+function replace_page_content(data, textStatus) {
+	/*************************************************************************
+	ajax success "handler".
+	replace the "#page_content" with the new html response data
+	*************************************************************************/
+	log("ajax post response success.");
+	log("status:" + textStatus);
+	if (data.indexOf("</body>") != -1) {
+		// FIXME: We should find a way to handle a 
+		// redirect directly. But we always get the
+		// html data of the redirected page.
+		
+		log("redirect work-a-round: replace the complete page");
+		log("</body> index:" + data.indexOf("</body>"));
+		replace_complete_page(data)
+	} else {
+		$("#page_content").html(data);
+		$("#page_content").animate({opacity: 1}, 500 );
+	}
+	load_normal_link = false;
+}
+
+
+function ajax_error_handler(XMLHttpRequest, textStatus, errorThrown) {
+	/*************************************************************************
+	ajax error "handler".
+	replace the complete page with the error text (django html traceback page)
+	*************************************************************************/
+	log("ajax get response error!");
+	log(XMLHttpRequest);
+	var response_text = XMLHttpRequest.responseText;
+	log("response_text: '" + response_text + "'");
+	if (!response_text) {
+		response_text = "<h1>Ajax response error without any response text.</h1>";
+	}
+	replace_complete_page(response_text);
+	load_normal_link = true;
+}
+
+
 function pylucid_ajax_form_view(form_id) {
     /*************************************************************************
     PyLucid ajax form view.
@@ -42,35 +89,32 @@ function pylucid_ajax_form_view(form_id) {
         var url = encodeURI(form.attr('action'));
         log("send form to url:" + url);
         
-        var load_normal_link = true;
+        load_normal_link = true;
         
-        $.ajax({
+        XMLHttpRequest = $.ajax({
         	async: false,
             type: "POST",
             url: url,
             data: form_data,
             dataType: "html",
             
-            success: function(form_html){
-        		log("ajax post response success.");
-                $("#page_content").html(form_html);
-                $("#page_content").animate({opacity: 1}, 500 );
-                load_normal_link = false;
+            success: replace_page_content,
+            complete: function(XMLHttpRequest, textStatus){
+            	// Handle redirects
+            	log("complete:" + XMLHttpRequest);
+            	log("text:" + textStatus);
+            	log("complete:" + XMLHttpRequest.status);
+            	log("complete:" + XMLHttpRequest.getResponseHeader('Location'));
+            	
+                if(XMLHttpRequest.status.toString()[0]=='3'){
+                	top.location.href = XMLHttpRequest.getResponseHeader('Location');
+                }
             },
-            error: function(XMLHttpRequest){
-            	log("ajax get response error!");
-                // Display the complete Traceback html page
-            	log(XMLHttpRequest);
-            	var response_text = XMLHttpRequest.responseText;
-            	log("response_text: '" + response_text + "'");
-            	if (!response_text) {
-            		document.write("<h1>ajax response error</h1>");
-            	} else {
-            		document.write(response_text);
-            	}
-            	load_normal_link = true;
-            }
-        });       
+            error: ajax_error_handler
+        });
+        log("ajax done:" + XMLHttpRequest);
+    	log("ajax done:" + XMLHttpRequest.status);
+    	log("ajax done:" + XMLHttpRequest.getResponseHeader('Location'));
         return load_normal_link; // <-- important: Don't send the form in normal way.
     }); 
 }
@@ -103,7 +147,7 @@ function get_pylucid_ajax_view(url) {
     var url = encodeURI(url);
     log("get:" + url);
     
-    var load_normal_link = true;
+    load_normal_link = true;
     
     $.ajax({
     	async: false,
@@ -111,25 +155,8 @@ function get_pylucid_ajax_view(url) {
         url: url,
         dataType: "html",
         
-        success: function(form_html){
-    		log("ajax get success.");
-            $("#page_content").html(form_html);
-            $("#page_content").animate({opacity: 1}, 500 );
-            load_normal_link = false;
-        },
-        error: function(XMLHttpRequest){
-        	log("ajax get response error!");
-            // Display the complete Traceback html page
-        	log(XMLHttpRequest);
-        	var response_text = XMLHttpRequest.responseText;
-        	log("response_text: '" + response_text + "'");
-        	if (!response_text) {
-        		document.write("<h1>ajax response error</h1>");
-        	} else {
-        		document.write(response_text);
-        	}
-        	load_normal_link = true;
-        }
+        success: replace_page_content,
+        error: ajax_error_handler
     });
     if (debug) {
     	// never fall back in debug mode.
