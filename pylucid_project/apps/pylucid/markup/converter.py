09466e37e5e017b92b49417322967fb1da52affc
JensDiemer <git@jensdiemer.de>
2009-07-14 14:12:04 +0000

    use PageContent model directly, so apply_markup can be used with other objects witch has the attribute "markup" and "content" ;)

--- pylucid_project/apps/pylucid/markup/converter.py
+++ pylucid_project/apps/pylucid/markup/converter.py
@@ -31,6 +31,8 @@ from django.utils.encoding import smart_str, force_unicode
 from pylucid.markup.django_tags import DjangoTagAssembler
 from pylucid_project.utils.SimpleStringIO import SimpleStringIO
 
+from pylucid.models import PageContent
+
 #______________________________________________________________________________
 # MARKUP
 
@@ -49,7 +51,7 @@ def fallback_markup(content):
     >>> fallback_markup("url: http://pylucid.org END")
     '<p>url: <a href="http://pylucid.org">pylucid.org</a> END</p>'
     """
-    content = content.replace("\r\n", "\n").replace("\r","\n")
+    content = content.replace("\r\n", "\n").replace("\r", "\n")
     blocks = BLOCK_RE.split(content)
     blocks = [line.replace("\n", "<br />\n") for line in blocks]
     content = "<p>" + "</p>\n\n<p>".join(blocks) + "</p>"
@@ -95,7 +97,7 @@ def apply_markdown(content, page_msg):
     else:
         # unicode support only in markdown v1.7 or above.
         # version_info exist only in markdown v1.6.2rc-2 or above.
-        if getattr(markdown, "version_info", None) < (1,7):
+        if getattr(markdown, "version_info", None) < (1, 7):
             return force_unicode(markdown.markdown(smart_str(content)))
         else:
             return markdown.markdown(content)
@@ -131,13 +133,13 @@ def apply_creole(content):
     into the generated page
     """
     from pylucid.markup import PyLucid_creole_macros
-    
+
     from creole import Parser
     from creole.creole2html import HtmlEmitter
-   
+
     # Create document tree from creole markup
     document = Parser(content).parse()
-    
+
     # Build html code from document tree
     return HtmlEmitter(document, macros=PyLucid_creole_macros, verbose=0).emit()
 
@@ -151,26 +153,26 @@ def apply_markup(pagecontent, page_msg):
     """
     markup_no = pagecontent.markup
     raw_content = pagecontent.content
-    
-    assemble_tags = markup_no not in (pagecontent.MARKUP_HTML, pagecontent.MARKUP_HTML_EDITOR)
+
+    assemble_tags = markup_no not in (PageContent.MARKUP_HTML, PageContent.MARKUP_HTML_EDITOR)
     if assemble_tags:
         # cut out every django tags from content
         assembler = DjangoTagAssembler()
         raw_content2, cut_data = assembler.cut_out(raw_content)
-    
-    if markup_no == pagecontent.MARKUP_TINYTEXTILE: # PyLucid's TinyTextile
+
+    if markup_no == PageContent.MARKUP_TINYTEXTILE: # PyLucid's TinyTextile
         html_content = apply_tinytextile(raw_content2, page_msg)
-        
-    elif markup_no == pagecontent.MARKUP_TEXTILE: # Textile (original)
+
+    elif markup_no == PageContent.MARKUP_TEXTILE: # Textile (original)
         html_content = apply_textile(raw_content2, page_msg)
-        
-    elif markup_no == pagecontent.MARKUP_MARKDOWN:
+
+    elif markup_no == PageContent.MARKUP_MARKDOWN:
         html_content = apply_markdown(raw_content2, page_msg)
-        
-    elif markup_no == pagecontent.MARKUP_REST:
+
+    elif markup_no == PageContent.MARKUP_REST:
         html_content = apply_restructuretext(raw_content2, page_msg)
-        
-    elif markup_no == pagecontent.MARKUP_CREOLE:
+
+    elif markup_no == PageContent.MARKUP_CREOLE:
         html_content = apply_creole(raw_content2)
 
     if assemble_tags:
