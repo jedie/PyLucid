df3ec28965e495ef9f41c98e38a826080a6d52d9
JensDiemer <git@jensdiemer.de>
2009-08-05 12:57:48 +0000

    superfish menu: no delay on mouseout

--- pylucid_project/media/PyLucid/superfish/pylucid_superfish.js
+++ pylucid_project/media/PyLucid/superfish/pylucid_superfish.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   // initialise Superfish menu
   // 
   $('ul.sf-menu').superfish({ 
-      delay:       1000,                            // one second delay on mouseout 
+      delay:       0,                               // delay on mouseout 
       animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
       speed:       'fast'                           // faster animation speed
   });
