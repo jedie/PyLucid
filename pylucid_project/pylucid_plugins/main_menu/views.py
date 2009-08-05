1b97bd9a12f65d941917a684377356fc1ba6a5ca
JensDiemer <git@jensdiemer.de>
2009-08-05 08:16:57 +0000

    bugfix: if current PageTree.showlinks==False -> go recursive up to next parent and use the first pagetree witch has showlinks==True.

--- pylucid_project/pylucid_plugins/main_menu/views.py
+++ pylucid_project/pylucid_plugins/main_menu/views.py
@@ -18,6 +18,7 @@
 
 __version__ = "$Rev: 1934 $"
 
+from django.conf import settings
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 
@@ -38,7 +39,24 @@ def lucidTag(request, min=1, max=0):
     tree = PageTree.objects.get_tree(user, filter_showlinks=True)
 
     # activate the current pagetree node (for main menu template)
-    tree.set_current_node(current_pagetree.id)
+    def get_first_showlink(pagetree):
+        """ returns the first pagetree witch has showlinks==True, go recursive up to next parent """
+        if pagetree.showlinks == False and pagetree.parent != None:
+            return get_first_showlink(pagetree.parent)
+        return pagetree
+
+    first_showlink = get_first_showlink(current_pagetree)
+#    try:
+    tree.set_current_node(first_showlink.id)
+#    except KeyError, err:
+#        tree.set_current_node(None) # Root node
+#        if settings.DEBUG:
+#            request.page_msg.error("Can't activate menu item %r KeyError: %s" % (current_pagetree, err))
+#            request.page_msg("tree nodes: %r" % tree.nodes)
+#            request.page_msg("All PageTree:", PageTree.objects.all())
+#            request.page_msg("Current user:", request.user)
+#            request.page_msg("all accessible PageTree:", PageTree.objects.all_accessible(request.user).all())
+
 
     # add all PageMeta objects into tree
     queryset = PageMeta.objects.filter(lang=current_lang)
