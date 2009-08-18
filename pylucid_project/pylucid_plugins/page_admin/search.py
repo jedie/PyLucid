e4b0b8ca947ccee7416748c1aa299b59c868fbf3
JensDiemer <git@jensdiemer.de>
2009-08-18 06:40:52 +0000

    bugfix for model changes - ticket:301

--- pylucid_project/pylucid_plugins/page_admin/search.py
+++ pylucid_project/pylucid_plugins/page_admin/search.py
@@ -12,13 +12,13 @@ def get_search_results(request, search_languages, search_strings, search_results
     queryset = PageContent.objects
 
     # search only all pages from this site
-    queryset = queryset.filter(page__site=Site.objects.get_current())
+    queryset = queryset.filter(pagemeta__page__site=Site.objects.get_current())
 
     # Filter PageTree view permissions:
     if request.user.is_anonymous(): # Anonymous user are in no user group
-        queryset = queryset.filter(page__permitViewGroup=None)
+        queryset = queryset.filter(pagemeta__page__permitViewGroup=None)
     elif not request.user.is_superuser: # Superuser can see everything ;)
-        queryset = queryset.filter(page__permitViewGroup__in=request.user.groups)
+        queryset = queryset.filter(pagemeta__page__permitViewGroup__in=request.user.groups)
 
     # Only pages in the selected search language
     queryset = queryset.filter(pagemeta__lang__in=search_languages)
