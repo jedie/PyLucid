98677baee0c6f1c6f5117e75988f2c7fa6fc4f38
JensDiemer <git@jensdiemer.de>
2009-08-20 13:14:05 +0000

    post_render_global_template is not used -> comment it out.

--- pylucid_project/apps/pylucid/signals.py
+++ pylucid_project/apps/pylucid/signals.py
@@ -3,4 +3,4 @@
 import django.dispatch
 
 pre_render_global_template = django.dispatch.Signal(providing_args=["request", "page_template"])
-post_render_global_template = django.dispatch.Signal(providing_args=["toppings", "size"])
+#post_render_global_template = django.dispatch.Signal(providing_args=["toppings", "size"])
