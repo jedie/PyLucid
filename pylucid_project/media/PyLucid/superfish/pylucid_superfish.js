d414d1e883e3291e5b5c965bc7d55c78f7d94bef
JensDiemer <git@jensdiemer.de>
2009-11-13 08:22:42 +0000

    Make superfish menu faster and add a small fade out delay.

--- pylucid_project/media/PyLucid/superfish/pylucid_superfish.js
+++ pylucid_project/media/PyLucid/superfish/pylucid_superfish.js
@@ -3,8 +3,8 @@ $(document).ready(function(){
   // initialise Superfish menu
   // 
   $('ul.sf-menu').superfish({ 
-      delay:       0,                               // delay on mouseout 
+      delay:       300,                             // delay on mouseout 
       animation:   {opacity:'show',height:'show'},  // fade-in and slide-down animation 
-      speed:       'fast'                           // faster animation speed
+      speed:       100                              // faster animation speed
   });
 });
