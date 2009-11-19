f468792a4e24322433599e8f334a54b7e8eba1c0
JensDiemer <git@jensdiemer.de>
2009-11-19 16:20:47 +0000

    log errors via LogEntry

--- pylucid_project/middlewares/pylucid_objects.py
+++ pylucid_project/middlewares/pylucid_objects.py
@@ -1,8 +1,17 @@
 # coding: utf-8
 
+import traceback
+
 from pylucid.system import pylucid_objects
+from pylucid.models import LogEntry
 
 class PyLucidMiddleware(object):
     def process_request(self, request):
         """ Add PyLucid objects to the request object """
         request.PYLUCID = pylucid_objects.PyLucidRequestObjects(request)
+
+    def process_exception(self, request, exception):
+        LogEntry.objects.log_action(
+            app_label="pylucid", action="PyLucidMiddleware.process_exception()", message=str(exception),
+            long_message=traceback.format_exc()
+        )
