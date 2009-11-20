22ccd753c04b1ab634607a3253d035b3817f5005
JensDiemer <git@jensdiemer.de>
2009-11-20 13:46:30 +0000

    Don't redirect to PyLucid own on site login/logout views, if any GET parameter given.

--- pylucid_project/apps/pylucid_admin/admin_site.py
+++ pylucid_project/apps/pylucid_admin/admin_site.py
@@ -13,6 +13,9 @@ class PyLucidAdminSite(admin.AdminSite):
     Own PyLucid Admin Site.
     """
     def logout(self, request):
+        if request.GET: # Don't redirect, if any GET parameter in url
+            return super(PyLucidAdminSite, self).logout(request)
+
         url = "/?" + settings.PYLUCID.AUTH_LOGOUT_GET_VIEW
         return HttpResponseRedirect(url)
 
@@ -20,9 +23,11 @@ class PyLucidAdminSite(admin.AdminSite):
         """
         redirect to PyLucid's own login view
         """
-        if PageTree.on_site.all().count() == 0 or UserProfile.on_site.all().count() == 0:
-            # The PyLucid inline login view does only work if e.g. a v0.8 migration starts
-            # Work-a-round: Use the normal django login view
+        if request.GET or PageTree.on_site.all().count() == 0 or UserProfile.on_site.all().count() == 0:
+            # Use the normal django login view
+            # This must be happend if PyLucid can't work.
+            # The User can also "deactivate" the redirect to PyLucid login view by adding any GET parameter
+            # e.g: domain.tld/admin/?foobar
             return super(PyLucidAdminSite, self).login(request)
 
         url = settings.PYLUCID.AUTH_NEXT_URL % {"path": "/", "next_url": request.get_full_path()}
