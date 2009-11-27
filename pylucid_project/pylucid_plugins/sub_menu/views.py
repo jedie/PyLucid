1726d637a8a6205a8bdd6f018995da37fcdd09e3
JensDiemer <git@jensdiemer.de>
2009-11-27 11:10:12 +0000

    bugfix for ticket:327 the sub_menu plugin fills entries in other languages

--- pylucid_project/pylucid_plugins/sub_menu/views.py
+++ pylucid_project/pylucid_plugins/sub_menu/views.py
@@ -19,22 +19,52 @@
 __version__ = "$Rev$"
 
 
-from pylucid.models import PageMeta
+from pylucid.models import PageTree, PageMeta, Language
 from pylucid.decorators import render_to
 
 
 @render_to("sub_menu/sub_menu.html")
 def lucidTag(request):
-    """
-    build the sub menu with all existing sub pages
-    TODO: filter showlinks and permit settings
-    TODO: sort by PageTree position!
-    """
-    # Get the current models.PageContent instance
-    pagetree = request.PYLUCID.pagetree
-    current_lang = request.PYLUCID.language_entry
+    """ build the sub menu with all existing sub pages"""
+    pagetree = request.PYLUCID.pagetree # current models.PageContent instance
+    current_lang = request.PYLUCID.language_entry # Client prefered language
+    default_lang = Language.objects.get_default() # System default language
 
-    sub_pages = PageMeta.objects.all().filter(pagetree__parent=pagetree, language=current_lang)
+    # Build a PageTree ID list of all accessible subpages
+    queryset = PageTree.objects.all_accessible(request.user, filter_showlinks=True)
+    queryset = queryset.filter(parent=pagetree)
+    sub_pagetree_ids = queryset.values_list("id", flat=True)
+
+    # Get PageMeta in client prefered language
+    queryset = PageMeta.objects.filter(language=current_lang)
+    pagemeta1 = queryset.filter(pagetree__in=sub_pagetree_ids).select_related()
+    existing_pagetree1 = [pagemeta.pagetree.id for pagemeta in pagemeta1]
+
+    # Build a list of all PageTree IDs witch haven't a PageMeta
+    missing_pagetree1 = [id for id in sub_pagetree_ids if id not in existing_pagetree1]
+    if not missing_pagetree1:
+        # No missing languages -> Use the source queryset
+        sub_pages = list(pagemeta1)
+    else: # Some pages didn't exist in current language -> use system default language
+        queryset = PageMeta.objects.filter(language=default_lang)
+        pagemeta2 = queryset.filter(pagetree__in=missing_pagetree1).select_related()
+
+        # PageMeta in current language + PageMeta in system default language
+        sub_pages = list(pagemeta1) + list(pagemeta2)
+
+        # Build a list of existing PageMeta
+        existing_pagetree2 = [pagemeta.pagetree.id for pagemeta in sub_pages]
+        # Exist there pages not in Client prefered language and not in System default language?
+        missing_pagetree2 = [id for id in sub_pagetree_ids if id not in existing_pagetree2]
+        if missing_pagetree2: # There are still missing PageMeta.
+            queryset = PageMeta.objects.exclude(language=current_lang).exclude(language=default_lang)
+            sub_pages3 = queryset.filter(pagetree__in=missing_pagetree2).select_related()
+
+            # merge sub pages
+            sub_pages += list(sub_pages3)
+
+    # sort by PageTree positions 
+    sub_pages.sort(cmp=lambda x, y: cmp(x.pagetree.position, y.pagetree.position))
 
     return {"sub_pages": sub_pages}
 
