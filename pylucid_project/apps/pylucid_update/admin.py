e0fa3a7d5418eee5dcbaba1c517e94ec991ed655
JensDiemer <git@jensdiemer.de>
2009-12-01 16:21:16 +0000

    Add v0.8 tables only in DEBUG mode to the django admin panel.

--- pylucid_project/apps/pylucid_update/admin.py
+++ pylucid_project/apps/pylucid_update/admin.py
@@ -4,7 +4,8 @@
     PyLucid.admin
     ~~~~~~~~~~~~~~
 
-    Register all old PyLucid model in django admin interface.
+    Register all old PyLucid model in django admin interface,
+    but only in DEBUG mode.
 
     Last commit info:
     ~~~~~~~~~~~~~~~~~
@@ -16,6 +17,7 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
+from django.conf import settings
 from django.contrib import admin
 
 from pylucid_project.apps.pylucid_update.models import Page08, Style08, Template08, \
@@ -24,93 +26,95 @@ from pylucid_admin.admin_site import pylucid_admin_site
 
 #------------------------------------------------------------------------------
 
-class JS_LoginDataAdmin(admin.ModelAdmin):
-    list_display = (
-        'user', 'sha_checksum', 'salt', 'createtime', 'lastupdatetime'
-    )
+if settings.DEBUG: # Add v0.8 tables only in DEBUG mode.
 
-pylucid_admin_site.register(JS_LoginData08, JS_LoginDataAdmin)
+    class JS_LoginDataAdmin(admin.ModelAdmin):
+        list_display = (
+            'user', 'sha_checksum', 'salt', 'createtime', 'lastupdatetime'
+        )
 
-
-class PageAdmin(admin.ModelAdmin):
-    list_display = (
-        "id", "shortcut", "name", "title", "description",
-        "lastupdatetime", "lastupdateby"
-    )
-    list_display_links = ("shortcut",)
-    list_filter = (
-        "createby", "lastupdateby", "permitViewPublic", "showlinks",
-        "template", "style", "markup",
-    )
-    date_hierarchy = 'lastupdatetime'
-    search_fields = ["content", "name", "title", "description", "keywords"]
-
-pylucid_admin_site.register(Page08, PageAdmin)
+    pylucid_admin_site.register(JS_LoginData08, JS_LoginDataAdmin)
 
 
-#class PluginAdmin(admin.ModelAdmin):
-#    list_display = (
-#        "active", "plugin_name", "description", "can_deinstall"
-#    )
-#    list_display_links = ("plugin_name",)
-#    ordering = ('package_name', 'plugin_name')
-#    list_filter = ("author","package_name", "can_deinstall")
-#
-#pylucid_admin_site.register(Plugin, PluginAdmin)
-#
-#
-#class PageArchivAdmin(admin.ModelAdmin):
-#    list_display = (
-#        "id", "pagetree", "edit_comment",
-#        "shortcut", "name", "title",
-#        "description", "lastupdatetime", "lastupdateby"
-#    )
-#
-#pylucid_admin_site.register(PageArchiv, PageArchivAdmin)
+    class PageAdmin(admin.ModelAdmin):
+        list_display = (
+            "id", "shortcut", "name", "title", "description",
+            "lastupdatetime", "lastupdateby"
+        )
+        list_display_links = ("shortcut",)
+        list_filter = (
+            "createby", "lastupdateby", "permitViewPublic", "showlinks",
+            "template", "style", "markup",
+        )
+        date_hierarchy = 'lastupdatetime'
+        search_fields = ["content", "name", "title", "description", "keywords"]
 
-#------------------------------------------------------------------------------
-
-#class PreferenceAdmin(admin.ModelAdmin):
-#    list_display = (
-#        "id", "plugin", "comment",
-#    )
-#    list_display_links = ("comment",)
-#    ordering = ("plugin", "id")
-#    list_filter = ("plugin",)
-#
-#pylucid_admin_site.register(Preference, PreferenceAdmin)
-
-#------------------------------------------------------------------------------
-
-class StyleAdmin(admin.ModelAdmin):
-    list_display = (
-        "id", "name", "description", "createtime", "lastupdatetime"
-    )
-    list_display_links = ("name",)
-    save_as = True
-
-pylucid_admin_site.register(Style08, StyleAdmin)
+    pylucid_admin_site.register(Page08, PageAdmin)
 
 
-class TemplateAdmin(admin.ModelAdmin):
-    list_display = ("id", "name", "description")
-    list_display_links = ("name",)
-    save_as = True
+    #class PluginAdmin(admin.ModelAdmin):
+    #    list_display = (
+    #        "active", "plugin_name", "description", "can_deinstall"
+    #    )
+    #    list_display_links = ("plugin_name",)
+    #    ordering = ('package_name', 'plugin_name')
+    #    list_filter = ("author","package_name", "can_deinstall")
+    #
+    #pylucid_admin_site.register(Plugin, PluginAdmin)
+    #
+    #
+    #class PageArchivAdmin(admin.ModelAdmin):
+    #    list_display = (
+    #        "id", "pagetree", "edit_comment",
+    #        "shortcut", "name", "title",
+    #        "description", "lastupdatetime", "lastupdateby"
+    #    )
+    #
+    #pylucid_admin_site.register(PageArchiv, PageArchivAdmin)
 
-pylucid_admin_site.register(Template08, TemplateAdmin)
+    #------------------------------------------------------------------------------
+
+    #class PreferenceAdmin(admin.ModelAdmin):
+    #    list_display = (
+    #        "id", "plugin", "comment",
+    #    )
+    #    list_display_links = ("comment",)
+    #    ordering = ("plugin", "id")
+    #    list_filter = ("plugin",)
+    #
+    #pylucid_admin_site.register(Preference, PreferenceAdmin)
+
+    #------------------------------------------------------------------------------
+
+    class StyleAdmin(admin.ModelAdmin):
+        list_display = (
+            "id", "name", "description", "createtime", "lastupdatetime"
+        )
+        list_display_links = ("name",)
+        save_as = True
+
+    pylucid_admin_site.register(Style08, StyleAdmin)
 
 
-#______________________________________________________________________________
-# Models from old Blog plugin
+    class TemplateAdmin(admin.ModelAdmin):
+        list_display = ("id", "name", "description")
+        list_display_links = ("name",)
+        save_as = True
 
-class BlogComment08Admin(admin.ModelAdmin):
-    pass
-pylucid_admin_site.register(BlogComment08, BlogComment08Admin)
+    pylucid_admin_site.register(Template08, TemplateAdmin)
 
-class BlogTagAdmin(admin.ModelAdmin):
-    pass
-pylucid_admin_site.register(BlogTag, BlogTagAdmin)
 
-class BlogEntryAdmin(admin.ModelAdmin):
-    pass
-pylucid_admin_site.register(BlogEntry, BlogEntryAdmin)
+    #______________________________________________________________________________
+    # Models from old Blog plugin
+
+    class BlogComment08Admin(admin.ModelAdmin):
+        pass
+    pylucid_admin_site.register(BlogComment08, BlogComment08Admin)
+
+    class BlogTagAdmin(admin.ModelAdmin):
+        pass
+    pylucid_admin_site.register(BlogTag, BlogTagAdmin)
+
+    class BlogEntryAdmin(admin.ModelAdmin):
+        pass
+    pylucid_admin_site.register(BlogEntry, BlogEntryAdmin)
