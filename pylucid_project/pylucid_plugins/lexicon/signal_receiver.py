d037c596d366980f8041f55d7acd826360f53ea5
JensDiemer <git@jensdiemer.de>
2010-03-04 19:51:54 +0000

    Escape the HTMLParser.HTMLParseError message. Add some DocStrings

--- pylucid_project/pylucid_plugins/lexicon/signal_receiver.py
+++ pylucid_project/pylucid_plugins/lexicon/signal_receiver.py
@@ -4,6 +4,9 @@
     PyLucid lexicon plugin
     ~~~~~~~~~~~~~~~~~~~~~~
 
+    signal receiver, connected in lexicon.__init__.py
+    Search in the page_content for all lexicon terms and
+    replace them with a link.
     
     Last commit info:
     ~~~~~~~~~
@@ -11,8 +14,8 @@
     $Rev: $
     $Author: JensDiemer $
 
-    :copyleft: 2009 by the PyLucid team, see AUTHORS for more details.
-    :license: GNU GPL v2 or above, see LICENSE for more details
+    :copyleft: 2009-2010 by the PyLucid team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details
 """
 
 __version__ = "$Rev:$"
@@ -20,16 +23,18 @@ __version__ = "$Rev:$"
 import sys
 import traceback
 import HTMLParser
+from xml.sax.saxutils import escape
 
 from django.conf import settings
 from django.utils.safestring import mark_safe
+from django.utils.translation import ugettext as _
 from django.template.loader import render_to_string
 
 # http://code.google.com/p/django-tagging/
 from tagging.utils import parse_tag_input
 
-from lexicon.models import LexiconEntry
-from lexicon.sub_html import SubHtml
+from pylucid_project.pylucid_plugins.lexicon.models import LexiconEntry
+from pylucid_project.pylucid_plugins.lexicon.sub_html import SubHtml
 
 
 WORD_TAG = u"<!-- word -->"
@@ -44,25 +49,31 @@ class LexiconData(dict):
         super(LexiconData, self).__init__(*args, **kwargs)
 
     def __call__(self, matchobject):
-        """ called from lexicon.sub_html.SubHtml """
-        word = matchobject.group(0)
-        data = self[word.lower()]
-        term = data["term"]
+        """
+        called from lexicon.sub_html.SubHtml
+        
+        We render a lexicon link in two steps, because the actual replaced word
+        for a term can be vary in spelling. So in the first step, we render
+        everything except the original word and cached this. In the second we
+        replaced the placeholder with the real word.
+        """
+        word = matchobject.group(0) # The original word
+        data = self[word.lower()] # get the lexicon data for this word
+        term = data["term"] # The lexicon term for this word
         if term not in self._cache:
             context = {
                 "term": term,
                 "short_definition": data["short_definition"],
             }
+            # Render the lexicon link without the original word.
             definition_link = render_to_string("lexicon/definition_link.html", context)
             self._cache[term] = definition_link
         else:
             definition_link = self._cache[term]
 
+        # Replace the placeholder with the original word.
         html = definition_link.replace(WORD_TAG, word)
         return html
-
-
-
 
 
 def pre_render_global_template_handler(**kwargs):
@@ -81,7 +92,7 @@ def pre_render_global_template_handler(**kwargs):
 
     lexicon_data = LexiconData()
     for term, alias, short_definition in entries:
-        aliases = parse_tag_input(alias) # Split django-tagging field value into a python list
+        aliases = parse_tag_input(alias) # Split django-tagging field value into a Python list
         all_words = [term] + aliases
         words_lower = set([word.lower() for word in all_words])
 
@@ -101,25 +112,25 @@ def pre_render_global_template_handler(**kwargs):
     try:
         s.feed(page_content)
     except HTMLParser.HTMLParseError, err:
-        # Base error message for all users:
-        msg = u"Wrong HTML code"
+        # HTMLParser can only parse valid HTML code.
+        err = escape(str(err))
+        msg = _("Wrong HTML code")
         if settings.DEBUG:
             # insert more information into the traceback and re-raise the original error
+            # XXX: http://code.djangoproject.com/ticket/13029 - Exception Value is empty in traceback page.
             etype, evalue, etb = sys.exc_info()
-            evalue = etype('%s (More info: %s)' % (msg, evalue))
+            evalue = etype('%s: %s' % (msg, err))
             raise etype, evalue, etb
 
-        if request.user.is_staff:
-            # add more info for staff members
+        if request.user.is_staff: # add more info for staff members
             msg += u" (%s)" % err
 
         if request.user.is_superuser:
             # put the full traceback into page_msg, but only for superusers
             request.page_msg(mark_safe("%s:<pre>%s</pre>" % (msg, traceback.format_exc())))
-
-        return
-
-    page_content = s.html
-    request.PYLUCID.context["page_content"] = mark_safe(page_content)
+    else:
+        page_content = s.html # Get the html code with the lexicon links
+        # Update the page content:
+        request.PYLUCID.context["page_content"] = mark_safe(page_content)
 
 
