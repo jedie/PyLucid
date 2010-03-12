5eba87f6509a47977fba8db70c36effa75014a3a
JensDiemer <git@jensdiemer.de>
2010-03-12 09:10:42 +0000

    update to new SHA implementation.

--- pylucid_project/pylucid_plugins/auth/js_sha_login_pseudocode.py
+++ pylucid_project/pylucid_plugins/auth/js_sha_login_pseudocode.py
@@ -1,8 +1,20 @@
-from md5 import new as md5_new
+#!/usr/bin/env python
+# coding: utf-8
 
+"""
+    Pseudo code of the JS-SHA-Login.
+    (output is in creole markup)
+    more info:
+    http://www.pylucid.org/permalink/42/secure-login-without-https
+"""
 
-def md5(txt):
-    return md5_new(txt).hexdigest()
+try:
+    from hashlib import sha1 as sha_constructor
+except ImportError:
+    from sha import new as sha_constructor
+
+def sha1(txt):
+    return sha_constructor(txt).hexdigest()
 
 def encrypt(txt, key): # Pseudo encrypt
     return "encrypted %s with %s" % (txt, key)
@@ -14,78 +26,79 @@ def decrypt(txt, key): # Pseudo decrypt
 
 
 
-print "\n\n------------ 1. Ein neuer User in der DB anlegen------------"
-print "\n 1.1. Server sendet salt zum Client:",
+print "\n\n=== 1. Create a new User ===\n"
+print "# Client get's new, random **user salt** from server:",
 salt = "s_123"
-print "'%s'" % salt
+print "'//%s//'" % salt
 
-print "\n 1.2. Eingabe des Passwortes auf dem Client:",
-password = "Passwort"
-print "'%s'" % password
+print "# Password input on the client:",
+password = "client_password"
+print "'//%s//'" % password
 
-print "\n 1.3. md5(password + salt):",
-md5sum = md5(password + salt)
-print "'%s'" % md5sum
+print "# sha1(password + salt):",
+sha1sum = sha1(password + salt)
+print "'//%s//'" % sha1sum
 
-print "\n 1.4. Übermittlung der MD5 Summe zum Server."
-
-
-
-print "\n\n------------ 2. speichern des Users auf dem Server------------"
-
-print "\n 2.1. Server trennt die MD5 in:",
-md5_a = md5sum[:16]
-md5_b = md5sum[16:]
-print "md5_a: '%s' md5_b: '%s'" % (md5_a, md5_b)
-
-print "\n 2.2. encrypt(md5_a, key=md5_b):",
-md5checksum = encrypt(md5_a, key=md5_b)
-print "'%s'" % md5checksum
-
-print "\n 2.3. Speichern nur der verschlüsselten Checksum + salt\n"
-
-
-print "_______________________________________________________________________"
+print "# Client send **sha1** hash to the server."
 
 
 
-print "\n\n------------ 3. Login eines Users------------"
+print "\n\n==== 2. Save user data ====\n"
 
-print "\n 3.1. Server sendet salt '%s' + challenge zum client:" % salt,
+print "# Server split sha1 values:",
+sha1_a = sha1sum[:16]
+sha1_b = sha1sum[16:]
+print "**sha1_a**: '//%s//' **sha1_b**: '//%s//'" % (sha1_a, sha1_b)
+
+print "# encrypt(sha1_a, key=sha1_b):",
+sha1checksum = encrypt(sha1_a, key=sha1_b)
+print "'//%s//'" % sha1checksum
+
+print "# Save only encrypted **checksum** + **user salt**\n"
+
+
+
+print "----"
+
+
+
+print "\n\n=== 3. Login ===\n"
+
+print "# Client request login and get's a random **challenge** from server:",
 challenge = "c_123"
-print "'%s'" % challenge
+print "'//%s//'" % challenge
 
-print "\n 3.2. Eingabe des Passwortes auf dem Client:",
-password = "Passwort"
-print "'%s'" % password
+print "# User enters username and password: '//%s//'" % password
 
-print "\n 3.3. md5(password + salt):",
-md5sum = md5(password + salt)
-print "'%s'" % md5sum
+print "# Client send username and get's **user salt** from server via AJAX: '//%s//'" % salt
 
-print "\n 3.4. trennen der MD5 in:",
-md5_a = md5sum[:16]
-md5_b = md5sum[16:]
-print "md5_a: '%s' md5_b: '%s'" % (md5_a, md5_b)
+print "# on the client: sha1(password + salt):",
+sha1sum = sha1(password + salt)
+print "'//%s//'" % sha1sum
 
-print "\n 3.5. md5_a2 = md5(md5_a + challenge):",
-md5_a2 = md5(md5_a + challenge)
-print "'%s'" % md5_a2
+print "# on the client: split sha1 in:",
+sha1_a = sha1sum[:16]
+sha1_b = sha1sum[16:]
+print "**sha1_a**: '//%s//' **sha1_b**: '//%s//'" % (sha1_a, sha1_b)
 
-print "\n 3.6. Übermittlung von md5_a2 und md5_b."
+print "# on the client: **sha1_a2** = sha1(sha1_a + challenge):",
+sha1_a2 = sha1(sha1_a + challenge)
+print "'//%s//'" % sha1_a2
+
+print "# Client send username, **sha1_a2** and **sha1_b** to the server."
 
 
 
-print "\n\n------------ 4. check auf dem Server------------"
+print "\n\n==== 4. validation on the server ====\n"
 
-print "\n 4.1. aus der DB md5checksum: '%s'" % md5checksum
+print "# get encrypted **checksum** for user: '//%s//'" % sha1checksum
 
-print "\n 4.2. decrypt(md5checksum, key=md5_b):",
-md5checksum = decrypt(md5checksum, key=md5_b)
-print "'%s'" % md5checksum
+print "# decrypt(sha1checksum, key=sha1_b):",
+sha1checksum = decrypt(sha1checksum, key=sha1_b)
+print "'//%s//'" % sha1checksum
 
-print "\n 4.3. md5(md5checksum + challenge):",
-md5check = md5(md5checksum + challenge)
-print "'%s'" % md5check
+print "# sha1(sha1checksum + challenge):",
+sha1check = sha1(sha1checksum + challenge)
+print "'//%s//'" % sha1check
 
-print "\n 4.4. Vergleich: %s == %s" % (md5check, md5_a2)
+print "# compare: //%s// == //%s//" % (sha1check, sha1_a2)
