6f82aea9ae42a8e9ecdeb96be1d49d81d5c40098
JensDiemer <git@jensdiemer.de>
2010-04-29 10:26:08 +0200

    cleanup manage.py for standalone package

--- scripts/standalone/manage.py
+++ scripts/standalone/manage.py
@@ -4,52 +4,18 @@
 """
     PyLucid - manage.py
     ~~~~~~~~~~~~~~~~~~~
-
-
+    
     http://docs.djangoproject.com/en/dev/ref/django-admin/
-
-    borrowed from the pinax project.
 """
 
-import os
-import sys
+from django.core.management import setup_environ, execute_from_command_line
 
-
-os.environ['DJANGO_SETTINGS_MODULE'] = "pylucid_project.settings"
-
-sys.stderr = sys.stdout
-
-
-def _error(msg):
-    print "Import Error:", msg
-    print "-" * 79
-    import traceback
-    traceback.print_exc()
-    print "-" * 79
-    print "Did you activate the virtualenv?"
-    sys.exit(1)
-
-try:
-    from django.core.management import setup_environ, execute_from_command_line
-except ImportError, msg:
-    _error(msg)
-
-
-try:
-    import pylucid_project
-except ImportError, msg:
-    _error(msg)
-
-
-try:
-    import settings as settings_mod
-except ImportError:
-    sys.stderr.write("Error: Can't import settings.py\n")
-    sys.exit(1)
-
+import pylucid_project # only a test, if exist
+from pylucid_project import settings as settings_mod
 
 # setup the environment before we start accessing things in the settings.
 setup_environ(settings_mod)
 
 if __name__ == "__main__":
     execute_from_command_line()
+
