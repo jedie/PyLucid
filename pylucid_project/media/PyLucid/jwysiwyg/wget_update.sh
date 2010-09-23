631e3065feeb5946e51791ee4d6fbaa1c080dc91
jens <jens@Athlon4600.(none)>
2010-09-24 00:12:57 +0200

    jquery.wysiwyg.jpg not needed

--- pylucid_project/media/PyLucid/jwysiwyg/wget_update.sh
+++ pylucid_project/media/PyLucid/jwysiwyg/wget_update.sh
@@ -1,5 +1,4 @@
 wget --timestamp http://github.com/akzhan/jwysiwyg/raw/master/jwysiwyg/jquery.wysiwyg.js
 wget --timestamp http://github.com/akzhan/jwysiwyg/raw/master/jwysiwyg/jquery.wysiwyg.css
 wget --timestamp http://github.com/akzhan/jwysiwyg/raw/master/jwysiwyg/jquery.wysiwyg.gif
-wget --timestamp http://github.com/akzhan/jwysiwyg/raw/master/jwysiwyg/jquery.wysiwyg.jpg
 wget --timestamp http://github.com/akzhan/jwysiwyg/raw/master/jwysiwyg/jquery.wysiwyg.modal.css
