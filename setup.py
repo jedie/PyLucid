3d2fe1c2b4940c646113719497c39005a4337f67
JensDiemer <git@jensdiemer.de>
2010-09-28 16:03:43 +0200

    * include media and locale files * catch errors in setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,7 @@
     PyLucid distutils setup
     ~~~~~~~~~~~~~~~~~~~~~~~
 
-    Last commit info:
-    ~~~~~~~~~~~~~~~~~
-    $LastChangedDate$
-    $Rev$
-    $Author$
-
-    :copyleft: 2009 by the PyLucid team, see AUTHORS for more details.
+    :copyleft: 2009-2010 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -27,16 +21,22 @@ PACKAGE_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 def get_authors():
-    f = file(os.path.join(PACKAGE_ROOT, "AUTHORS"), "r")
-    authors = [l.strip(" *\r\n") for l in f if l.strip().startswith("*")]
-    f.close()
+    try:
+        f = file(os.path.join(PACKAGE_ROOT, "AUTHORS"), "r")
+        authors = [l.strip(" *\r\n") for l in f if l.strip().startswith("*")]
+        f.close()
+    except Exception, err:
+        authors = "[Error: %s]" % err
     return authors
 
 
 def get_long_description():
-    f = file(os.path.join(PACKAGE_ROOT, "README"), "r")
-    long_description = f.read().strip()
-    f.close()
+    try:
+        f = file(os.path.join(PACKAGE_ROOT, "README.textile"), "r")
+        long_description = f.read().strip()
+        f.close()
+    except Exception, err:
+        long_description = "[Error: %s]" % err
     return long_description
 
 
