0adfdf8a98d6bbab12aef225ccc720fde2aaceb3
JensDiemer <git@jensdiemer.de>
2010-09-30 09:25:51 +0200

    don't move into project dir

--- scripts/manage.sh
+++ scripts/manage.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+# copy this file into: /home/FooBar/PyLucid_env/manage.sh
+
 function verbose_eval {
     echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     echo $*
     echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     eval $*
-    echo ---------------------------------------------------------------------
     echo
 }
 
@@ -14,9 +15,7 @@ echo activate the virtual environment:
 verbose_eval source bin/activate
 
 echo _____________________________________________________________________
-echo Go into source folder:
-verbose_eval cd src/pylucid/pylucid_project/
-
-echo _____________________________________________________________________
 echo execute manage.py
-verbose_eval python manage.py $*
+verbose_eval python src/pylucid/pylucid_project/manage.py $*
+
+echo ---------------------------------------------------------------------
