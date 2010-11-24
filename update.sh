d76095e32c1507b4a7994c1872ea2b3716fbae92
JensDiemer <git@jensdiemer.de>
2010-11-24 15:19:57 +0100

    simpler update.sh

--- update.sh
+++ update.sh
@@ -1,25 +1,17 @@
 #!/bin/bash
 
-echo
-
-function verbose_eval {
-    echo
-    echo _____________________________________________________________________
-    echo $*
-    echo - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-    eval $*
-    echo ---------------------------------------------------------------------
-    echo
-}
-
 if [ -e .git ];
 then
-    echo ".git directory found"
-    verbose_eval git pull origin master  
+    (
+        set -x
+        git pull origin master
+    )
 elif [ -e .svn ];
 then
-    echo ".svn directory found"
-    verbose_eval svn update
+    (
+        set -x
+        svn update
+    )
 else
     echo "no .git or .svn directory found. Can't update"
 fi
