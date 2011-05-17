b0ce724831c8586cd91cfedec34274db4cc01be7
JensDiemer <git@jensdiemer.de>
2011-05-17 11:16:46 +0200

    update Blueprint CSS Framework to v1.0.1

--- pylucid_project/media/PyLucid/blueprint/wget_update.sh
+++ pylucid_project/media/PyLucid/blueprint/wget_update.sh
@@ -1,3 +1,3 @@
-wget --timestamp http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/screen.css
-wget --timestamp http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/print.css
-wget --timestamp http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/ie.css
+wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/screen.css
+wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/print.css
+wget --timestamp --no-check-certificate http://github.com/joshuaclayton/blueprint-css/raw/master/blueprint/ie.css
