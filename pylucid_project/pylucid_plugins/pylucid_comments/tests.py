bab8121de2afb273493a8910dccce37e5469c952
JensDiemer <git@jensdiemer.de>
2011-05-18 12:32:09 +0200

    update pylucid comments unittests

--- pylucid_project/pylucid_plugins/pylucid_comments/tests.py
+++ pylucid_project/pylucid_plugins/pylucid_comments/tests.py
@@ -44,18 +44,18 @@ class PyLucidCommentsTestCase(basetest.BaseUnittest):
         d.update(f.initial)
         d.update(kwargs)
         return d
-        
+
 
 class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
-    
+
     def _pre_setup(self, *args, **kwargs):
         super(PyLucidCommentsPageMetaTest, self)._pre_setup(*args, **kwargs)
         self.pagemeta = PageMeta.on_site.all()[0]
         self.absolute_url = self.pagemeta.get_absolute_url()
-        
+
     def setUp(self):
         Comment.objects.all().delete()
-        
+
     def _get_form(self):
         url = self.absolute_url + "?pylucid_comments=get_form"
         data = self.getValidData(self.pagemeta)
@@ -67,39 +67,39 @@ class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
             HTTP_X_REQUESTED_WITH='XMLHttpRequest'
         )
         return response
-    
+
     def test_get_form(self):
         """ get the comment form via AJAX """
         response = self._get_form()
         self.assertResponse(response,
             must_contain=(
                 '<form action="JavaScript:void(0)" method="post" id="comment_form">',
-                '<input type="hidden" name="content_type" value="pylucid.pagemeta"',
-                '<input type="hidden" name="object_pk" value="%i"' % self.pagemeta.pk,
-                '<input checked="checked" type="checkbox" name="notify" id="id_notify" />'
+                '<input id="id_content_type" name="content_type" type="hidden" value="pylucid.pagemeta" />',
+                '<input id="id_object_pk" name="object_pk" type="hidden" value="%i" />' % self.pagemeta.pk,
+                '<input checked="checked" id="id_notify" name="notify" type="checkbox" />'
             ),
             must_not_contain=(
                 "Traceback", "Form errors", "field is required",
                 "<body", "</html>"
             )
         )
-    
+
     def test_submit_comment(self):
         settings.DEBUG = True # Display a comment error page
         self.failUnless(Comment.objects.count() == 0)
         self.failUnless(len(mail.outbox) == 0, len(mail.outbox))
         url = self.absolute_url + "?pylucid_comments=submit"
-        
+
         # submit a valid comments form
         data = self.getValidData(self.pagemeta, comment="from test_submit_comment()")
         response = self.client.post(url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        
+
         # Check if comment created
         self.failUnless(Comment.objects.count() == 1)
-        
+
         # Check if page should reload (JavaScript do this)
         self.failUnlessEqual(response.content, 'reload')
-        
+
         # Check if ADMINS get's a email.
         #for email in mail.outbox:print email.message()
         if len(mail.outbox) > 1:
@@ -112,47 +112,47 @@ class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
         self.failUnless(data["name"] not in email_text)
         self.failUnless(data["comment"] not in email_text)
         self.failUnless("http://testserver%s" % self.absolute_url not in email_text)
-        
+
         # Check if anonymous data saved in a cookie, for later usage
         self.failUnless("comments_data" in response.cookies)
         comments_data = response.cookies["comments_data"].value
         self.failUnless(comments_data.startswith("John Doe;john.doe@example.tld;"))
-        
+
         # Check if anonymous data stored in cookie would be used:
         response = self._get_form()
         self.assertResponse(response,
             must_contain=(
-                '<input id="id_name" type="text" name="name" value="John Doe" maxlength="50" />',
-                '<input type="text" name="email" value="john.doe@example.tld" id="id_email" />',
+                '<input id="id_name" maxlength="50" name="name" type="text" value="John Doe" />',
+                '<input id="id_email" name="email" type="text" value="john.doe@example.tld" />',
             )
         )
-        
+
     def test_submit_preview(self):
         settings.DEBUG = True # Display a comment error page
         self.failUnless(Comment.objects.count() == 0)
         url = self.absolute_url + "?pylucid_comments=submit"
-        data = self.getValidData(self.pagemeta, preview="On", comment="comment from test_submit_preview()")        
+        data = self.getValidData(self.pagemeta, preview="On", comment="comment from test_submit_preview()")
         response = self.client.post(url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.assertResponse(response,
             must_contain=(
                 'Preview your comment',
                 '<blockquote><p>comment from test_submit_preview()</p></blockquote>',
                 '<form action="JavaScript:void(0)" method="post" id="comment_form">',
-                '<input type="hidden" name="content_type" value="pylucid.pagemeta"',
-                '<input type="hidden" name="object_pk" value="%i"' % self.pagemeta.pk,
-                'type="checkbox" name="notify" id="id_notify" />',
+                '<input id="id_content_type" name="content_type" type="hidden" value="pylucid.pagemeta" />',
+                '<input id="id_object_pk" name="object_pk" type="hidden" value="%i" />' % self.pagemeta.pk,
+                '<input id="id_notify" name="notify" type="checkbox" />'
             ),
             must_not_contain=(
                 "Traceback", "Form errors", "field is required",
                 "<body", "</html>"
             )
         )
-    
+
     def test_submit_no_comment(self):
         settings.DEBUG = True # Display a comment error page
         self.failUnless(Comment.objects.count() == 0)
         url = self.absolute_url + "?pylucid_comments=submit"
-        data = self.getValidData(self.pagemeta, comment="")
+        data = self.getValidData(self.pagemeta, comment="", notify="on")
         response = self.client.post(url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
         self.failUnless(Comment.objects.count() == 0)
         self.assertResponse(response,
@@ -160,13 +160,13 @@ class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
                 'Please correct the error below',
                 'This field is required.',
                 '<form action="JavaScript:void(0)" method="post" id="comment_form">',
-                '<input type="hidden" name="content_type" value="pylucid.pagemeta"',
-                '<input type="hidden" name="object_pk" value="%i"' % self.pagemeta.pk,
-                'type="checkbox" name="notify" id="id_notify" />',
+                '<input id="id_content_type" name="content_type" type="hidden" value="pylucid.pagemeta" />',
+                '<input id="id_object_pk" name="object_pk" type="hidden" value="%i" />' % self.pagemeta.pk,
+                '<input checked="checked" id="id_notify" name="notify" type="checkbox" value="on" />'
             ),
             must_not_contain=("Traceback", "<body", "</html>")
         )
-    
+
     def test_submit_spam(self):
         settings.DEBUG = True # Display a comment error page
         self.failUnless(Comment.objects.count() == 0)
@@ -175,7 +175,7 @@ class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
             comment="Penis enlargement pills: http://en.wikipedia.org/wiki/Penis_enlargement ;)"
         )
         response = self.client.post(url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        
+
         # Check if page should reload (JavaScript do this)
         self.failUnlessEqual(response.content, 'reload')
 
@@ -196,7 +196,7 @@ class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
         self.failUnless(Comment.objects.count() == 1)
         comment = Comment.objects.all()[0]
         self.failUnless(comment.is_public == False)
-        
+
         # 'Reload' and check page message
         response = self.client.get(self.absolute_url)
         self.assertResponse(response,
@@ -208,37 +208,37 @@ class PyLucidCommentsPageMetaTest(PyLucidCommentsTestCase):
                 "Penis", "enlargement", "pills",
             )
         )
-        
+
     def test_DOS_attack(self):
         settings.DEBUG = True # Display a comment error page
         self.failUnless(Comment.objects.count() == 0)
         url = self.absolute_url + "?pylucid_comments=submit"
-              
+
         preferences = _get_preferences()
         ban_limit = preferences["ban_limit"]
-        
+
         # Hold if all three events would been received. 
         tested_under_limit = False
         tested_limit_reached = False
         tested_banned = False
-        
-        for no in xrange(1, ban_limit+2):          
+
+        for no in xrange(1, ban_limit + 2):
             # submit a valid comments form
             data = self.getValidData(self.pagemeta, comment="test_DOS_attack() comment no %i" % no)
             response = self.client.post(url, data, HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-            
-            if no>ban_limit:
+
+            if no > ban_limit:
                 # IP is on the ban list
                 tested_banned = True
                 self.assertStatusCode(response, 403) # get forbidden page
                 comment_count = Comment.objects.count()
-                self.failUnless(comment_count == ban_limit-1)
-            elif no==ban_limit:
+                self.failUnless(comment_count == ban_limit - 1)
+            elif no == ban_limit:
                 # The limit has been reached
                 tested_limit_reached = True
                 self.assertResponse(response, must_contain=('Add IP to ban list.',))
                 comment_count = Comment.objects.count()
-                self.failUnless(comment_count == ban_limit-1)
+                self.failUnless(comment_count == ban_limit - 1)
             else:
                 # under ban limit: comment was saved, page should be reloaded
                 tested_under_limit = True
