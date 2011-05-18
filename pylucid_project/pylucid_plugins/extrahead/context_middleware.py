c2b9c7d89db4e3a5f1556097effccdef59785679
JensDiemer <git@jensdiemer.de>
2011-05-18 13:47:42 +0200

    code cleanup

--- pylucid_project/pylucid_plugins/extrahead/context_middleware.py
+++ pylucid_project/pylucid_plugins/extrahead/context_middleware.py
@@ -1,5 +1,6 @@
 # coding: utf-8
 
+
 """
     PyLucid extrahead context middleware
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -12,17 +13,10 @@
     PyLucid plugins should use {% extrahead %} block tag (pylucid.defaulttags.extraheadBlock)
     in plugin template for insert e.g. CSS/JS file links into html head.
 
-    Last commit info:
-    ~~~~~~~~~
-    $LastChangedDate: $
-    $Rev: $
-    $Author: JensDiemer $
-
     :copyleft: 2009 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details
 """
 
-__version__= "$Rev:$"
 
 import os
 import inspect
@@ -35,21 +29,21 @@ class ContextMiddleware(object):
     def __init__(self, request, context):
         self.request = request
         self.extrahead = request.PYLUCID.extrahead # pylucid.system.extrahead
-                            
+
     def _add_pagetree_headfiles(self):
         """ add all headfile links used in the current design. """
         pagetree = self.request.PYLUCID.pagetree
         design = pagetree.design
-        
+
         colorscheme = design.colorscheme
         headfiles = design.headfiles.all()
-        
+
         for headfile in headfiles:
             # Get a instance from pylucid_project.apps.pylucid.system.headfile.HeadfileLink():
             headfilelink = headfile.get_headfilelink(colorscheme)
             head_tag = headfilelink.get_head_tag()
             self.extrahead.append(head_tag)
-        
+
     def render(self):
         """ return all extra head content with all headfiles from current used design """
         self._add_pagetree_headfiles()
