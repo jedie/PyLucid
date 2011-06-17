6ef727a7f0db48126f534df1dc287b36e75fca92
JensDiemer <git@jensdiemer.de>
2011-06-17 15:50:29 +0200

    code clenup

--- pylucid_project/apps/pylucid/models/__init__.py
+++ pylucid_project/apps/pylucid/models/__init__.py
@@ -2,13 +2,7 @@
 
 """
     PyLucid.models
-    ~~~~~~~~~~~~~~~~~~~
-
-    Last commit info:
-    ~~~~~~~~~~~~~~~~~
-    $LastChangedDate: $
-    $Rev: $
-    $Author: $
+    ~~~~~~~~~~~~~~
 
     :copyleft: 2009 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
