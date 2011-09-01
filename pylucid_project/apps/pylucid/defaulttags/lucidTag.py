433c43f512f2651afdf6cba3c58becd681f74ee6
Jens Diemer <github.com@jensdiemer.de>
2011-09-01 09:36:21 +0300

    Edited pylucid_project/apps/pylucid/defaulttags/lucidTag.py via GitHub

--- pylucid_project/apps/pylucid/defaulttags/lucidTag.py
+++ pylucid_project/apps/pylucid/defaulttags/lucidTag.py
@@ -9,12 +9,6 @@
 
     registered in: ./PyLucid/defaulttags/__init__.py
 
-    Last commit info:
-    ~~~~~~~~~~~~~~~~~
-    $LastChangedDate$
-    $Rev$
-    $Author$
-
     :copyleft: 2007 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
