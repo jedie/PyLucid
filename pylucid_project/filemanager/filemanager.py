67b24e495118f20f761f70c2e008e40a17f84d25
JensDiemer <git@jensdiemer.de>
2012-06-29 17:55:58 +0200

    start splitting the filemanager functionality from gallery plugin to create a base for a static file manager, see: https://github.com/jedie/PyLucid/issues/73 http://forum.pylucid.org/viewtopic.php?f=4&t=344 (de)

---
+++ pylucid_project/filemanager/filemanager.py
@@ -0,0 +1,112 @@
+# coding: utf-8
+
+"""
+    filemanager
+    ~~~~~~~~~~~    
+    
+    :copyleft: 2012 by the django-tools team, see AUTHORS for more details.
+    :license: GNU GPL v3 or above, see LICENSE for more details.
+"""
+
+
+import os
+import posixpath
+
+from django.conf import settings
+from django.http import Http404
+from django.utils.translation import ugettext as _
+
+
+DEFAULT_UNAUTHORIZED_SIGNS = ("..", "//", "\\")
+
+
+class FilemanagerError(Exception):
+    """
+    for errors with a message to staff/admin users.
+    e.g.: Gallery filesystem path doesn't exist anymore.
+    """
+    pass
+
+
+class DirectoryTraversalAttack(FilemanagerError):
+    """
+    Some unauthorized signs are found or the path is out of the base path.
+    """
+    pass
+
+
+class Filemanager(object):
+    """
+    Base class for a django app like a filemanager.
+    """
+    def __init__(self, request, absolute_path, base_url, rest_url, unauthorized_signs = None):
+        """
+        absolute_path - path in filesystem to the root directory
+        base_url - url prefix of this filemanager instance
+        rest_url - relative sub path of the current view
+        
+        it is assumed that 'absolute_path' and 'base_url' are internal values
+        and 'rest_url' are a external given value from the requested user.
+        """
+        self.request = request
+        
+        # characters that aren't allowed in the path.
+        # To protect from: https://en.wikipedia.org/wiki/Directory_traversal_attack
+        if unauthorized_signs is None:
+            self.unauthorized_signs = DEFAULT_UNAUTHORIZED_SIGNS
+        else:
+            self.unauthorized_signs = unauthorized_signs
+        assert isinstance(unauthorized_signs, (list, tuple)), "'unauthorized_signs' must be a list or tuples!"
+        
+        self.absolute_path = absolute_path
+        self.base_url = base_url
+        
+        self.check_unauthorized_sign(rest_url)
+        self.rel_path = os.path.normpath(rest_url)
+        self.rel_url = posixpath.normpath(self.rel_path)
+
+        self.abs_path = os.path.normpath(os.path.join(self.absolute_path, self.rel_path))
+        self.check_unauthorized_sign(self.abs_path) # not really necessary
+        
+        self.abs_url = posixpath.normpath(posixpath.join(self.base_url, self.rel_url))
+        
+        if not os.path.isdir(self.abs_path):
+            raise FilemanagerError("Builded path %r doesn't exist." % self.abs_path)
+
+    def build_breadcrumbs(self):
+        parts = ""
+        url = self.base_url
+        breadcrumbs = [{
+            "name": _("index"),
+            "title": _("goto 'index'"),
+            "url": url
+        }]
+        rel_url = self.rel_url.strip("/")
+        if not rel_url:
+            return breadcrumbs
+
+        for url_part in rel_url.split("/"):
+            url += "%s/" % url_part
+            parts += "%s/" % url_part
+            breadcrumbs.append({
+                "name": url_part,
+                "title": _("goto '%s'") % parts,
+                "url": url
+            })
+        return breadcrumbs
+
+    def check_unauthorized_sign(self, value):
+        for sign in self.unauthorized_signs:
+            print value, sign
+            if sign and sign in value:
+                raise DirectoryTraversalAttack("%r found in %s" % (sign, repr(value)))
+
+    def check_path(self, base_path, path):
+        """
+        Simple check if the path is a sub directory of base_path.
+        This must be called from external!
+        """
+        if not path.startswith(base_path):
+            raise DirectoryTraversalAttack("%r doesn't start with %r" % (path, base_path))
+
+
--- pylucid_project/pylucid_plugins/gallery/views.py
+++ pylucid_project/pylucid_plugins/gallery/views.py
@@ -5,7 +5,7 @@
     PyLucid gallery
     ~~~~~~~~~~~~~~~
     
-    :copyleft: 2010-2011 by the PyLucid team, see AUTHORS for more details.
+    :copyleft: 2010-2012 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
@@ -27,6 +27,9 @@ from django.core.urlresolvers import reverse
 from django.http import Http404, HttpResponseRedirect, HttpResponseBadRequest
 from django.template.context import RequestContext
 from django.utils.translation import ugettext as _
+
+# TODO: Should be moved to django-tools!
+from pylucid_project.filemanager.filemanager import Filemanager, FilemanagerError
 
 from pylucid_project.apps.pylucid.models import LogEntry
 from pylucid_project.apps.pylucid.shortcuts import render_pylucid_response
@@ -72,100 +75,29 @@ def _split_suffix(filename, suffix_list):
 #------------------------------------------------------------------------------
 
 
-class GalleryError(Exception):
-    """
-    for errors with a message to staff/admin users.
-    e.g.: Gallery filesystem path doesn't exist anymore.
-    """
-    pass
+class Gallery(Filemanager):
+    def __init__(self, config, *args, **kwargs):
+        # use unauthorized signs from preferences
+        pref_form = GalleryPrefForm()
+        preferences = pref_form.get_preferences()
+        unauthorized_signs = preferences["unauthorized_signs"]
+        kwargs["unauthorized_signs"] = unauthorized_signs
+        
+        super(Gallery, self).__init__(*args, **kwargs)
 
+        # Galleries are only allowed in STATIC_ROOT
+        self.check_path(settings.STATIC_ROOT, self.abs_path)
 
-class Gallery(object):
-    def __init__(self, request, config, rest_url):
-        self.request = request
-        self.pagetree = self.request.PYLUCID.pagetree
         self.config = config
-
-        self.gallery_base_url = self.request.PYLUCID.pagemeta.get_absolute_url()
-
-        self.rel_path, self.rel_url = self.check_rest_url(rest_url)
-
-        self.rel_base_path = self.config.path
-        self.abs_path = os.path.normpath(os.path.join(settings.STATIC_ROOT, self.rel_base_path, self.rel_path))
-
-        is_dir = os.path.isdir(self.abs_path)
-        is_in_root = self.abs_path.startswith(settings.STATIC_ROOT)
-        if not is_dir or not is_in_root:
-            msg = _("Wrong path.")
-            has_change_perm = request.user.has_perm("gallery.change_gallerymodel")
-            if settings.DEBUG or has_change_perm:
-                msg += " - path %r" % self.abs_path
-                if not is_dir:
-                    msg += " is not a directory."
-                if not is_in_root:
-                    msg += " is not in media root."
-                if has_change_perm: # raise 404 in debug
-                    raise GalleryError(msg)
-            raise Http404(msg)
-
-        self.abs_base_url = posixpath.normpath(posixpath.join(settings.STATIC_URL, self.rel_base_path, self.rel_path))
-
+        self.static_base_url = posixpath.normpath(posixpath.join(settings.STATIC_URL, config.path, self.rel_url))
+        
         dirs, pictures, thumbs = self.read_dir(self.abs_path)
 
         self.dir_info = self.build_dir_info(dirs)
         self.picture_info = self.build_picture_info(pictures, thumbs)
 
         self.breadcrumbs = self.build_breadcrumbs()
-
-    def build_breadcrumbs(self):
-        parts = ""
-        url = self.gallery_base_url
-        breadcrumbs = [{
-            "name": _("index"),
-            "title": _("goto 'index'"),
-            "url": url
-        }]
-        rel_url = self.rel_url.strip("/")
-        if not rel_url:
-            return breadcrumbs
-
-        for url_part in rel_url.split("/"):
-            url += "%s/" % url_part
-            parts += "%s/" % url_part
-            breadcrumbs.append({
-                "name": url_part,
-                "title": _("goto '%s'") % parts,
-                "url": url
-            })
-        return breadcrumbs
-
-    def make_url(self, part):
-        return posixpath.normpath(posixpath.join(self.abs_base_url, part))
-
-    def check_rest_url(self, rest_url):
-        if rest_url == "":
-            return ("", "")
-
-        pref_form = GalleryPrefForm()
-        preferences = pref_form.get_preferences()
-        unauthorized_signs = preferences["unauthorized_signs"]
-        assert isinstance(unauthorized_signs, (list, tuple))
-        for sign in unauthorized_signs:
-            if sign and sign in rest_url:
-                msg1 = "unauthorized sign"
-                msg = "'%s' in '%s'" % (sign, rest_url)
-                LogEntry.objects.log_action(
-                    app_label="pylucid_plugin.gallery", action=msg1, message=msg
-                )
-                msg404 = "bad path"
-                if settings.DEBUG or self.request.user.is_staff:
-                    msg404 += " - %s: %s" % (msg1, msg)
-                raise Http404(msg404)
-
-        rel_path = os.path.normpath(rest_url)
-        rel_url = posixpath.normpath(rel_path)
-        return rel_path, rel_url
-
+        
     def read_dir(self, path):
         pictures = []
         thumbs = {}
@@ -211,6 +143,9 @@ class Gallery(object):
 
         return dir_info
 
+    def make_url(self, part):
+        return posixpath.normpath(posixpath.join(self.static_base_url, part))
+
     def build_picture_info(self, pictures, thumbs):
         picture_info = []
         for picture in pictures:
@@ -232,7 +167,7 @@ class Gallery(object):
 
             picture_info.append(info)
         return picture_info
-
+    
     def render(self):
         context = {
             "a_rel_info": self.breadcrumbs[-1]["name"],
@@ -246,7 +181,7 @@ class Gallery(object):
         return render_pylucid_response(self.request, self.config.template, context,
             context_instance=RequestContext(self.request)
         )
-
+        
 
 def gallery(request, rest_url=""):
     pagetree = request.PYLUCID.pagetree
@@ -266,11 +201,22 @@ def gallery(request, rest_url=""):
                 parent_url = pagetree.parent.get_absolute_url()
             return HttpResponseRedirect(parent_url)
 
+    rel_base_path = config.path
+    base_url = request.PYLUCID.pagemeta.get_absolute_url()
+    absolute_path = os.path.normpath(os.path.join(settings.STATIC_ROOT, rel_base_path))
+
     try:
-        gallery = Gallery(request, config, rest_url)
-    except GalleryError, err:
-        messages.error(request, "Gallery error: %s" % err)
-        return HttpResponseRedirect(reverse("admin:gallery_gallerymodel_change", args=(config.id,)))
+        gallery = Gallery(config, request, absolute_path, base_url, rest_url)
+    except FilemanagerError, err:
+        has_change_perm = request.user.has_perm("gallery.change_gallerymodel")
+        if settings.DEBUG or has_change_perm:
+            raise
+        
+        LogEntry.objects.log_action(
+            app_label="pylucid_plugin.gallery", action="build gallery", message="%s" % err
+        )
+        raise Http404("Gallery error.")
+
 
     if not request.is_ajax():
         # FIXME: In Ajax request, only the page_content would be replaced, not the
