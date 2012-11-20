fbc9120c933b41a7bbfad80522dffb619547774c
JensDiemer <git@jensdiemer.de>
2012-11-20 11:40:44 +0100

    Add a flickr rss solution in generic plugin. More info: http://www.pylucid.org/permalink/360/generic-plugin

--- pylucid_project/pylucid_plugins/generic/views.py
+++ pylucid_project/pylucid_plugins/generic/views.py
@@ -6,14 +6,142 @@
     
     Simple rendering templates with some variables.
 
-    :copyleft: 2010 by the PyLucid team, see AUTHORS for more details.
+    :copyleft: 2010-2012 by the PyLucid team, see AUTHORS for more details.
     :license: GNU GPL v3 or above, see LICENSE for more details
 """
 
+import sys
+import time
+import traceback
+
 from django.conf import settings
 from django.contrib import messages
+from django.core.cache import cache
+from django.utils import simplejson as json
+from django.utils.safestring import mark_safe
 
 from pylucid_project.apps.pylucid.decorators import render_to
+from pylucid_project import VERSION_STRING
+from pylucid_project.utils.escape import escape
+
+from django_tools.utils.http import HttpRequest
+
+
+
+def _error(request, msg, staff_msg):
+    etype, value, tb = sys.exc_info()
+    if tb is not None:
+#        if settings.DEBUG and settings.RUN_WITH_DEV_SERVER:
+#            raise
+        if request.user.is_superuser:
+            # put the full traceback into page_msg, but only for superusers
+            messages.debug(request,
+                mark_safe(
+                    "%s:<pre>%s</pre>" % (escape(staff_msg), escape(traceback.format_exc()))
+                )
+            )
+        return "[%s]" % msg
+
+    if request.user.is_staff:
+        messages.error(request, staff_msg)
+
+    return "[%s]" % msg
+
+
+def get_json_remote(request, url, cache_time, timeout, encoding=None):
+    """
+    include a remote file into a page.
+    Arguments, see DocString of lucidTag()
+    """
+    cache_key = "get_json_remote_%s" % url
+    context = cache.get(cache_key)
+    if context:
+        from_cache = True
+    else:
+        from_cache = False
+
+        # Get the current page url, for referer
+        context = request.PYLUCID.context
+        page_absolute_url = context["page_absolute_url"]
+        current_language = request.PYLUCID.current_language
+        current_language_code = current_language.code
+
+        user_agent = "PyLucid CMS v%s" % VERSION_STRING
+
+        start_time = time.time()
+        try:
+            # Request the url content in unicode
+            r = HttpRequest(url, timeout=timeout, threadunsafe_workaround=True)
+            r.request.add_header("User-agent", user_agent)
+            r.request.add_header("Referer", page_absolute_url)
+            r.request.add_header("Accept-Language", current_language_code)
+
+            response = r.get_response()
+            raw_content = r.get_unicode()
+        except Exception, err:
+            return _error(request, "Remote error.", "Can't get %r: %s" % (url, err))
+
+        duration = time.time() - start_time
+
+        # get request/response information
+        request_header = response.request_header
+        response_info = response.info()
+
+        raw_json = raw_content[len("jsonFlickrFeed("):-1]
+        try:
+            data = json.loads(raw_json)
+        except Exception, err:
+            _error(request, "Remote error.", "Can't load json %r: %s" % (url, err))
+            data = None
+        else:
+            # Insert alternate picture sizes
+            for pic in data["items"]:
+                url = pic["media"][u"m"]
+                for char in (u"s", u"q", u"t", u"n", u"z", u"c", u"b"):
+                    if not char in pic["media"]:
+                        pic["media"][char] = url.replace(u"_m.jpg", u"_%s.jpg" % char)
+
+        context = {
+            "url": url,
+            "raw_content": raw_content,
+            "data":data,
+            "request_header": request_header,
+            "response_info": response_info,
+            "duration": duration,
+            "cache_time":cache_time,
+            "timeout": timeout,
+        }
+        cache.set(cache_key, context, cache_time)
+
+    context["from_cache"] = from_cache
+    return context
+
+
+@render_to()
+def flickr_rss(request, id=None, template_name="generic/flickr_colorbox.html", max=None, cache_time=60, timeout=5, debug=False, **kwargs):
+    url = "https://secure.flickr.com/services/feeds/photos_public.gne?format=json"
+    if id:
+        url += "&id=%s" % id
+
+    current_language = request.PYLUCID.current_language
+    current_language_code = current_language.code
+    url += "&lang=%(l)s-%(l)s" % {"l":current_language_code}
+
+    context = get_json_remote(request, url, cache_time, timeout)
+    if isinstance(context, basestring):
+        # Error message
+        return context
+
+    if max and context["data"]:
+        context["data"]["items"] = context["data"]["items"][:max]
+
+    context.update({
+        "template_name":template_name,
+        "max": max,
+        "debug": debug,
+    })
+    context.update(kwargs)
+    return context
 
 
 @render_to()
@@ -47,6 +175,7 @@ def lucidTag(request, **context):
     Available boilerplate:
     * YouTube
     * ohloh
+    * flickr
     
     more info:
     http://www.pylucid.org/permalink/360/generic-plugin
@@ -56,6 +185,8 @@ def lucidTag(request, **context):
         {% lucidTag generic.youtube id="XL1UNmLDLKc" width=960 height=745 %}
         {% lucidTag generic.ohloh project="pylucid" %}
         {% lucidTag generic.ohloh project="python" js_file="project_users.js?style=rainbow" %}
+        {% lucidTag generic.flickr_rss %}
+        {% lucidTag generic.flickr_rss id="12345678@N90" max=5 %}
         {% lucidTag generic template_name="myowntemplate.html" %}
     """
     if "template_name" not in context and (request.user.is_staff or settings.DEBUG):
