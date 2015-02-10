3e3fac926b278d37ad7373c9c60e2f71d6b818b0
JensDiemer <git@jensdiemer.de>
2015-02-10 11:52:14 +0100

    write design files

--- pylucid_migration/management/commands/export_designs.py
+++ pylucid_migration/management/commands/export_designs.py
@@ -1,12 +1,17 @@
 import re
 import os
+import shutil
 import unicodedata
 
 from django.core.management.base import BaseCommand, CommandError
+from django.template import loader, TemplateDoesNotExist
+from django.template.loaders import app_directories
+
 from pylucid_migration.models import Design, PageTree, DBTemplate
 
 
-BASE_PATH=os.path.join(".", "exported")
+
+PATH_EXPORTED=os.path.join(".", "design_exported")
 
 
 def clean_string(value):
@@ -15,17 +20,51 @@ def clean_string(value):
     return re.sub('[_\s]+', '_', value)
 
 
+class Loader(app_directories.Loader):
+    is_usable = True
+
 
 class Command(BaseCommand):
     args = '<poll_id poll_id ...>'
     help = 'Closes the specified poll for voting'
 
+    def _process_template(self, design, base_out_path):
+        template_filename = design.template
+
+        try:
+            dbtemplate = DBTemplate.objects.get(name=template_filename)
+        except DBTemplate.DoesNotExist:
+            self.stdout.write("\tINFO: %r is not a dbtemplate" % template_filename)
+            try:
+                content, origin = Loader().load_template_source(template_filename)
+            except TemplateDoesNotExist:
+                self.stderr.write("ERROR: Template not found: %r" % template_filename)
+                return
+        else:
+            content = dbtemplate.content
+
+        filepath=os.path.join(base_out_path, template_filename)
+        self._write_file(filepath, content)
+
+    def _process_headfiles(self, design, base_out_path):
+        for headfile in design.headfiles.all():
+            # self.stdout.write("\t\t%r" % headfile)
+            if headfile.render:
+                # CSS ColorScheme entries in the content
+                colorscheme = design.colorscheme
+                content = headfile.get_rendered(colorscheme)
+            else:
+                content = headfile.content
+
+            headfile_path = os.path.join(base_out_path, headfile.filepath)
+            self._write_file(headfile_path, content)
+
     def handle(self, *args, **options):
         for design in Design.objects.all():
             clean_design_name = clean_string(design.name)
             # raise CommandError('Poll "%s" does not exist' % poll_id)
-            self.stdout.write("\n\t*** %r" % clean_design_name)
-            self.stdout.write("\t\t%r" % design)
+            self.stdout.write("\n*** %r ***" % clean_design_name)
+            self.stdout.write("\t%r" % design)
 
             sites = design.sites.all()
             count_info=[]
@@ -39,50 +78,27 @@ class Command(BaseCommand):
 
             clean_site_name = clean_string(most_used_site.name)
 
-            base_out_filename = os.path.join(BASE_PATH, clean_site_name, clean_design_name)
-            self.stdout.write("\t\t%r" % base_out_filename)
+            base_out_path = os.path.join(PATH_EXPORTED, clean_site_name, clean_design_name)
 
-            self._verbose_makedirs(base_out_filename)
+            shutil.rmtree(base_out_path, ignore_errors=True) # TODO: remove later!!!
 
-            template_filename = design.template
+            self._process_headfiles(design, base_out_path)
+            self._process_template(design, base_out_path)
 
-            try:
-                dbtemplate = DBTemplate.objects.get(name=template_filename)
-            except DBTemplate.DoesNotExist:
-                self.stdout.write("\tINFO: %r is not a dbtemplate" % template_filename)
-                # TODO: Try to use normal template loading
-            else:
-                self.stdout.write("\t\t%r" % dbtemplate)
-
-            # TODO: continue here:
-
-
-            for headfile in design.headfiles.all():
-                self.stdout.write("\t\t%r" % headfile)
-                headfile_path = os.path.join(base_out_filename, headfile.filepath)
-
-                self.stdout.write("\t\t%r" % headfile_path)
-
-                if headfile.render:
-                    # CSS ColorScheme entries in the content
-                    colorscheme = design.colorscheme
-                    content = headfile.get_rendered(colorscheme)
-                else:
-                    content = headfile.content
-
-
-                # self.stdout.write(content)
-                # self.stdout.write("-"*60)
-
-
-
+    def _write_file(self, filepath, content):
+        self._verbose_makedirs(filepath)
+        self.stdout.write("\tcreate %r..." % filepath)
+        with open(filepath, "w") as f:
+            f.write(content)
 
     def _verbose_makedirs(self, path):
-            try:
-                os.makedirs(path)
-            except FileExistsError:
-                self.stdout.write("Path %r already exists, ok." % path)
-            else:
-                self.stdout.write("Path %r created, ok." % path)
+        path=os.path.dirname(path)
+        try:
+            os.makedirs(path)
+        except FileExistsError:
+            pass
+        #     self.stdout.write("\t\tPath %r already exists, ok." % path)
+        # else:
+        #     self.stdout.write("\t\tPath %r created, ok." % path)
 
 
