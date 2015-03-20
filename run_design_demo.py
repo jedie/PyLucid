b3557c1167ca8241ff3a5f5d036c590b15d817bd
JensDiemer <git@jensdiemer.de>
2015-03-20 20:49:01 +0100

    start "design_demo" as default

--- run_design_demo.py
+++ run_design_demo.py
@@ -8,8 +8,12 @@
 """
 
 from __future__ import unicode_literals, print_function
+import sys
 
 from pylucid_design_demo.manage_design_demo import cli
 
 if __name__ == "__main__":
+    if len(sys.argv)==1:
+        sys.argv.append("design_demo")
+        
     cli()
