be5a62a56223472f0b21bfae8c5933bbbe072bd2
JensDiemer <git@jensdiemer.de>
2015-04-30 22:44:46 +0200

    better preview in structure view

--- pylucid_todo/models.py
+++ pylucid_todo/models.py
@@ -10,7 +10,17 @@
 """
 
 from django.db import models
+from django.utils.safestring import mark_safe
+from django.utils.html import strip_tags
+from django.utils.text import Truncator
+
+from cms.utils.compat.dj import python_2_unicode_compatible
 from cms.models import CMSPlugin
 
+@python_2_unicode_compatible
 class ToDoPlugin(CMSPlugin):
     code = models.TextField()
+
+    def __str__(self):
+        content = Truncator(strip_tags(self.code)).chars(30, html=True)
+        return mark_safe(content)
