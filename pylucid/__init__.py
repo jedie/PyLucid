b674d8a5ef250d7e7aa89a6c4769fed8f78a2be6
Jens Diemer <git@jensdiemer.de>
2015-07-13 14:10:19 +0200

    fixup! update setup.py publish from:
    https://gist.github.com/jedie/e356a0c17ac1a2fcb716

--- pylucid/__init__.py
+++ pylucid/__init__.py
@@ -13,11 +13,4 @@
 
 from __future__ import absolute_import, division, print_function
 
-__version__ = (2,0,0,"beta")
-
-
-VERSION_STRING = '.'.join(str(part) for part in __version__)
-
-
-if __name__ == "__main__":
-    print(VERSION_STRING)
+__version__ = "2.0.0.beta"
