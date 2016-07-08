352504579e34a7017e2a93b8ca3f444e673d1cff
JensDiemer <git@jensdiemer.de>
2016-07-08 18:16:33 +0200

    use :memory:

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -9,18 +9,11 @@
     :license: GNU GPL v3 or above, see LICENSE for more details.
 """
 
-import os
-import tempfile
-
 from pylucid_installer.page_instance_template.example_project.settings import *
-
-TEMP_DIR = tempfile.mkdtemp(prefix="pylucid_unittest_")
-DATABASE_NAME=os.path.join(TEMP_DIR, "pylucid_unittest_database.sqlite3")
-print("Test database: %r" % DATABASE_NAME)
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': DATABASE_NAME,
+        'NAME': ":memory:"
     }
 }
