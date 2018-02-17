8bd032e8abb007ce449efd52af08cdb651a1202e
JensDiemer <git@jensdiemer.de>
2018-02-17 18:14:31 +0100

    WIP: create venv

--- pylucid/pylucid_admin.py
+++ pylucid/pylucid_admin.py
@@ -6,6 +6,14 @@
 
     A interactive admin for PyLucid.
 
+    usage, e.g.:
+
+        $ wget https://raw.githubusercontent.com/jedie/PyLucid/pylucid_v3/pylucid/pylucid_admin.py
+        $ python3 pylucid_admin.py
+
+        pylucid_admin.py> boot ~/PyLucid_env
+
+
     :created: 08.02.2018 by Jens Diemer, www.jensdiemer.de
     :copyleft: 2018 by the PyLucid team, see AUTHORS for more details.
     :license: GNU General Public License v3 or later (GPLv3+), see LICENSE for more details.
@@ -19,6 +27,19 @@ import subprocess
 import sys
 import traceback
 from pathlib import Path
+
+try:
+    import venv
+except ImportError as err:
+    # e.g.: debian / ubuntu doesn't have venv installed, isn't it?!?
+    print("\nERROR: 'venv' not available: %s (Maybe 'python3-venv' package not installed?!?)" % err)
+
+try:
+    import ensurepip
+except ImportError as err:
+    # e.g.: debian / ubuntu doesn't have venv installed, isn't it?!?
+    print("\nERROR: 'ensurepip' not available: %s (Maybe 'python3-venv' package not installed?!?)" % err)
+
 
 __version__ = "0.0.1"
 
@@ -109,8 +130,10 @@ class Cmd2(cmd.Cmd):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # e.g.: $ ./pylucid.py upgrade_requirements -> run do_upgrade_requirements() on startup
-        self.cmdqueue = sys.argv[1:]
+        # e.g.: $ pylucid_admin.py boot /tmp/PyLucid-env -> run self.do_boot("/tmp/PyLucid-env") on startup
+        args = sys.argv[1:]
+        if args:
+            self.cmdqueue = [" ".join(args)]
 
     @display_errors
     def _complete_path(self, text, line, begidx, endidx):
@@ -181,6 +204,53 @@ class Cmd2(cmd.Cmd):
         return stop
 
 
+class PyLucidEnvBuilder(venv.EnvBuilder):
+    verbose = True
+
+    def ensure_directories(self, env_dir):
+        print(" * Create the directories for the environment.")
+        return super().ensure_directories(env_dir)
+    
+    def create_configuration(self, context):
+        print(" * Create 'pyvenv.cfg' configuration file.")
+        return super().create_configuration(context)
+    
+    def setup_python(self, context):
+        print(" * Set up a Python executable in the environment.")
+        return super().setup_python(context)
+    
+    def _setup_pip(self, context):
+        print(" * Installs or upgrades pip in a virtual environment.")
+        return super()._setup_pip(context)
+    
+    def setup_scripts(self, context):
+        print(" * Set up scripts into the created environment.")
+        return super().setup_scripts(context)
+        
+    def post_setup(self, context):
+        """
+        Set up any packages which need to be pre-installed into the
+        virtual environment being created.
+
+        :param context: The information for the virtual environment
+                        creation request being processed.
+        """
+        print(" * post-setup modification")
+
+        os.environ['VIRTUAL_ENV'] = context.env_dir
+
+        pip3_path = Path(context.bin_path, "pip3")
+        if not pip3_path.is_file():
+            print("ERROR: pip not found here: '%s'" % pip3_path)
+            return
+
+        print("pip found here: '%s'" % pip3_path)
+        context.pip3_path = str(pip3_path)
+
+        verbose_check_call(context.pip3_path, "install", "--upgrade", "pip")
+        verbose_check_call(context.pip3_path, "install", "django<2.0")
+
+
 
 
 class PyLucidShell(Cmd2):
@@ -207,7 +277,18 @@ class PyLucidShell(Cmd2):
             self.stdout.write("\nERROR: Path '%s' already exists!\n" % destination)
             return
 
-        print("TODO: boot install to: '%s'" % destination)
+        self.stdout.write("Create virtualenv: '%s'...\n\n" % destination)
+
+        builder = PyLucidEnvBuilder(with_pip=True)
+        builder.create(destination)
+
+        self.stdout.write("\n")
+
+        if not destination.is_dir():
+            self.stdout.write("ERROR: Creating virtualenv!\n")
+            return
+        else:
+            self.stdout.write("virtualenv created at: '%s'\n" % destination)
 
     #_________________________________________________________________________
     # Developer commands:
