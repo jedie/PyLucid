1b472ae47a6c78d28e31dafc83d7b11d4149da6f
JensDiemer <git@jensdiemer.de>
2018-02-17 14:50:56 +0100

    append piprot information unter *.txt requirement files

--- pylucid.py
+++ pylucid.py
@@ -8,15 +8,18 @@
     :copyleft: 2018 by the PyLucid team, see AUTHORS for more details.
     :license: GNU General Public License v3 or later (GPLv3+), see LICENSE for more details.
 """
-
-__version__ = "0.0.1"
-
 import argparse
 import cmd
+import glob
 import logging
 import os
 import subprocess
 import sys
+
+
+
+__version__ = "0.0.1"
+
 
 if sys.version_info < (3, 5):
     print("\nERROR: Python 3.5 or greater is required!\n")
@@ -27,8 +30,11 @@ log = logging.getLogger(__name__)
 
 SELF_FILENAME=os.path.basename(__file__)
 
+
 def verbose_check_call(*args):
-    """ 'verbose' version of subprocess.check_output() """
+    """
+    'verbose' version of subprocess.check_output()
+    """
     print("Call: %r" % " ".join(args))
     try:
         subprocess.check_call(args, universal_newlines=True, stderr=subprocess.STDOUT)
@@ -36,6 +42,22 @@ def verbose_check_call(*args):
         print("\n***ERROR:")
         print(err.output)
         raise
+
+
+def iter_subprocess_output(args):
+    """
+    A subprocess with tee ;)
+    """
+    print("Call: %s" % " ".join(args))
+
+    env = dict(os.environ)
+    env["PYTHONUNBUFFERED"]="1" # If a python script called ;)
+
+    proc=subprocess.Popen(args,
+        stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
+        bufsize=1, env=env, universal_newlines=True,
+    )
+    return iter(proc.stdout.readline,'')
 
 
 class Cmd2(cmd.Cmd):
@@ -108,21 +130,40 @@ class Cmd2(cmd.Cmd):
         return stop
 
 
+
+
+
+
 class PyLucidShell(Cmd2):
     def do_upgrade_requirements(self, arg):
         """
-        run pip-compile for all *.in files
+        Convert via 'pip-compile' *.in requirements files to *.txt
+        Append 'piprot' informations to *.txt requirements.
 
         Direct start with:
             $ ./pylucid.py upgrade_requirements
         """
-        for filename in os.listdir("requirements"):
-            if not filename.endswith(".in") or filename.startswith("basic"):
+        for requirement_in in glob.glob(os.path.join("requirements", "*.in")):
+            if "basic_" in requirement_in:
                 continue
-            requirement_in = os.path.join("requirements", filename)
             requirement_out = requirement_in.replace(".in", ".txt")
 
+            self.stdout.write("_"*79 + "\n")
             verbose_check_call("pip-compile", "--verbose", "--upgrade", "-o", requirement_out, requirement_in)
+
+            self.stdout.write("_"*79 + "\n")
+            args = ["piprot", "--outdated", requirement_out]
+            output = [
+                "\n#\n# list of out of date packages made with piprot:\n#\n"
+            ]
+            for line in iter_subprocess_output(args):
+                self.stdout.write(line)
+                self.stdout.flush()
+                output.append("# %s" % line)
+
+            self.stdout.write("\nUpdate file %r\n" % requirement_out)
+            with open(requirement_out, "a") as f:
+                f.writelines(output)
 
     def _install(self, requirements_filename):
         verbose_check_call("pip3", "install", "--upgrade", "pip")
