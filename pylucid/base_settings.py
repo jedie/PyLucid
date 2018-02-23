0b92a5c4cc3e0a97967f6dc872151ebb7e704e36
JensDiemer <git@jensdiemer.de>
2018-02-23 21:13:56 +0100

    update base settings and use it

--- pylucid/base_settings.py
+++ pylucid/base_settings.py
@@ -3,64 +3,108 @@
 """
     PyLucid base settings
     ~~~~~~~~~~~~~~~~~~~~~
-
-    see also:
-    https://github.com/django/django/blob/master/django/conf/global_settings.py
-    https://docs.djangoproject.com/en/1.8/ref/settings/
 """
 
-import os
+from pathlib import Path
 
 from django.utils.translation import ugettext_lazy as _
-# _ = lambda s: s
 
 
-ALLOWED_HOSTS = []
+# For build paths inside the project:
+BASE_DIR = Path(__file__).resolve().parent
 
-TIME_ZONE = 'America/Chicago'
-LANGUAGES = (
-    ('en', _('English')),
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'Only for the tests ;)'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+SITE_ID=1
+
+
+from django_tools.settings_utils import FnMatchIps
+
+# Required for the debug toolbar to be displayed:
+INTERNAL_IPS = FnMatchIps(["localhost", "127.0.0.1", "::1", "172.*.*.*", "192.168.*.*", "10.0.*.*"])
+
+ALLOWED_HOSTS = INTERNAL_IPS
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'djangocms_admin_style', # must be inserted before django.contrib.admin
+
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.sites',
+
+    'debug_toolbar', # https://github.com/jazzband/django-debug-toolbar/
+
+    'compressor', # https://github.com/django-compressor/django-compressor
+
+    'cms', # https://github.com/divio/django-cms
+    'menus', # Part of Django-CMS
+
+    'djangocms_history', # https://github.com/divio/djangocms-history
+
+    'meta', # https://github.com/nephila/django-meta
+
+    'easy_thumbnails', # https://github.com/SmileyChris/easy-thumbnails
+    'treebeard', # https://github.com/django-treebeard/django-treebeard
+    'sekizai', # https://github.com/ojii/django-sekizai
+    'djangocms_text_ckeditor', # https://github.com/divio/djangocms-text-ckeditor
+
+    # https://pypi.org/project/django-parler
+    'parler',
+
+    'filer', # https://github.com/divio/django-filer
+
+    # TODO: remove if migration from 'cmsplugin_filer_link' to 'djangocms-link' is done:
+    # http://docs.django-cms.org/en/latest/topics/commonly_used_plugins.html#deprecated-addons
+    'cmsplugin_filer_link',
+
+    'cmsplugin_pygments', # https://github.com/chrisglass/cmsplugin-pygments
+
+    # https://github.com/jedie/django-cms-tools/
+    'django_cms_tools',
+    'django_cms_tools.filer_tools',
+    'django_cms_tools.plugin_anchor_menu',
+    # 'django_cms_tools.plugin_landing_page', # TODO: Needs publisher!
+
+    'pylucid',
+    # 'pylucid_todo', # Only needed for old migrated installations
+
+    # Installed only in developer installation:
+    #'django_extensions', # https://github.com/django-extensions/django-extensions
 )
-LANGUAGE_CODE = 'en'
-USE_I18N = True
-USE_L10N = True
 
-# If you set this to True, Django will use timezone-aware datetimes:
-USE_TZ = False
+ROOT_URLCONF = 'pylucid_page_instance.urls'
+WSGI_APPLICATION = 'pylucid_page_instance.wsgi.application'
 
-DEBUG = False
-
-INTERNAL_IPS = (
-    '127.0.0.1',
-    '::1',
-)
-
-
-CACHES = {
-    'default': {
-        # https://docs.djangoproject.com/en/1.8/topics/cache/#database-caching
-        # manage.py createcachetable
-        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
-        'LOCATION': 'cache_table',
-    },
-}
-
-SITE_ID = 1
-
-
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = (
     'django.middleware.cache.UpdateCacheMiddleware',
 
-    'cms.middleware.utils.ApphookReloadMiddleware',
+    # https://github.com/jazzband/django-debug-toolbar/
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 
+    'cms.middleware.utils.ApphookReloadMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'django.middleware.security.SecurityMiddleware',
     'cms.middleware.user.CurrentUserMiddleware',
     'cms.middleware.page.CurrentPageMiddleware',
     'cms.middleware.toolbar.ToolbarMiddleware',
@@ -69,103 +113,147 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.cache.FetchFromCacheMiddleware',
 )
 
-TEMPLATES = [{
-    'BACKEND': 'django.template.backends.django.DjangoTemplates',
-    'DIRS': [],
-    'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-            'django.core.context_processors.i18n',
-            'django.core.context_processors.debug',
-            'django.core.context_processors.request',
-            'django.core.context_processors.media',
-            'django.core.context_processors.csrf',
-            'django.core.context_processors.tz',
-            'sekizai.context_processors.sekizai',
-            'django.core.context_processors.static',
-            'cms.context_processors.cms_settings',
-            'pylucid.context_processors.pylucid',
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            str(Path(BASE_DIR, "templates/")),
         ],
+        'OPTIONS': {
+            'loaders': [
+                ('django.template.loaders.cached.Loader', (
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                )),
+            ],
+            'context_processors': [
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.template.context_processors.media',
+                'django.template.context_processors.csrf',
+                'django.template.context_processors.tz',
+                'sekizai.context_processors.sekizai',
+                'django.template.context_processors.static',
+                'cms.context_processors.cms_settings',
+                'pylucid.context_processors.pylucid',
+            ],
+        },
     },
-},]
+]
 
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': str(Path(BASE_DIR, '..', 'test_project_db.sqlite3').resolve()),
+        # 'NAME': ":memory:"
+    }
+}
+
+
+# https://docs.djangoproject.com/en/1.11/topics/cache/#database-caching
+# manage.py createcachetable
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'pylucid_cache_table',
+    },
+}
+# Hack needed, until https://github.com/divio/django-cms/issues/5079 is fixed:
+import sys
+if "createcachetable" in sys.argv:
+    INSTALLED_APPS = ()
+
+
+# https://django-compressor.readthedocs.io/en/latest/settings/
+#COMPRESS_ENABLED=False
+COMPRESS_ENABLED=True
+
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    # other finders..
+    'compressor.finders.CompressorFinder',
+)
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.11/topics/i18n/
+
+# Default and fallback language:
+# https://docs.djangoproject.com/en/1.11/ref/settings/#language-code
+LANGUAGE_CODE = "en"
+
+# http://django-parler.readthedocs.org/en/latest/quickstart.html#configuration
+PARLER_LANGUAGES = {
+    1: [
+        {
+            "name": _("German"),
+            "code": "de",
+            "fallbacks": [LANGUAGE_CODE],
+            "hide_untranslated": False,
+        },
+        {
+            "name": _("English"),
+            "code": "en",
+            "fallbacks": ["de"],
+            "hide_untranslated": False,
+        },
+    ],
+    "default": { # all SITE_ID"s
+        "fallbacks": [LANGUAGE_CODE],
+        "redirect_on_fallback": False,
+    },
+}
+
+
+# https://docs.djangoproject.com/en/1.11/ref/settings/#languages
+# http://www.i18nguy.com/unicode/language-identifiers.html
+LANGUAGES = tuple([(d["code"], d["name"]) for d in PARLER_LANGUAGES[1]])
+
+LANGUAGE_DICT = dict(LANGUAGES) # useful to get translated name by language code
+
+# http://docs.django-cms.org/en/latest/reference/configuration.html#std:setting-CMS_LANGUAGES
+# CMS_LANGUAGES = PARLER_LANGUAGES
+
+# http://django-parler.readthedocs.org/en/latest/quickstart.html#configuration
+PARLER_DEFAULT_LANGUAGE_CODE = LANGUAGE_CODE
+
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.8/howto/static-files/
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = str(Path(BASE_DIR, 'static'))
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT = str(Path(BASE_DIR, 'media'))
 
-STATICFILES_FINDERS = (
-    "compressor.finders.CompressorFinder",
-    "django.contrib.staticfiles.finders.FileSystemFinder",
-    "django.contrib.staticfiles.finders.AppDirectoriesFinder"
-)
 
-INSTALLED_APPS = (
-    'djangocms_admin_style', # must be inserted before django.contrib.admin
+# https://django-debug-toolbar.readthedocs.io/en/stable/configuration.html#debug-toolbar-config
+from debug_toolbar.settings import CONFIG_DEFAULTS as DEBUG_TOOLBAR_CONFIG
 
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.admin',
-    'django.contrib.sites',
-    'django.contrib.sitemaps',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
+# don't load jquery from ajax.googleapis.com, just use django's version:
+DEBUG_TOOLBAR_CONFIG["JQUERY_URL"] = STATIC_URL + "admin/js/vendor/jquery/jquery.min.js"
 
-    'pylucid',
 
-    'cms',
-
-    'djangocms_text_ckeditor',
-    'menus',
-    'sekizai',
-    'treebeard',
-
-    # https://github.com/stefanfoulis/django-filer
-    'filer',
-    'easy_thumbnails',
-    # https://github.com/stefanfoulis/cmsplugin-filer
-    'cmsplugin_filer_file',
-    'cmsplugin_filer_folder',
-    'cmsplugin_filer_link',
-    'cmsplugin_filer_image',
-    'cmsplugin_filer_teaser',
-    'cmsplugin_filer_utils',
-    'cmsplugin_filer_video',
-
-    'djangocms_htmlsitemap', # https://github.com/kapt-labs/djangocms-htmlsitemap
-    'cmsplugin_pygments', # https://github.com/chrisglass/cmsplugin-pygments
-    'cmsplugin_markup', # https://github.com/mitar/cmsplugin-markup
-
-    'reversion', # https://github.com/etianen/django-reversion
-    'reversion_compare', # https://github.com/jedie/django-reversion-compare
-    'compressor', # https://github.com/django-compressor/django-compressor
-
-    # djangocms-blog
-    'parler',
-    'taggit',
-    'taggit_autosuggest',
-    'meta',
-    'meta_mixin',
-    'aldryn_apphooks_config',
-    'djangocms_blog',
-
-    'djangocms_widgets',
-    'djangocms_widgets_socialshareprivacy',
-
-    # Installed only in developer installation:
-    #'django_extensions', # https://github.com/django-extensions/django-extensions
-)
-
-# djangocms_widgets
-WIDGET_TEMPLATES = (
-    ('socialshareprivacy.html', 'SocialSharePrivacy'), # djangocms_widgets_socialshareprivacy
-)
+# Basic Django CMS settings
 
 CMS_TEMPLATES = (
     ('pylucid/bootstrap/split_tree_menu_left.html', 'Split tree menu left'),
@@ -175,77 +263,34 @@ CMS_TEMPLATES = (
     ('pylucid/bootstrap/tree_menu_left.html', 'Tree menu left'),
     ('pylucid/bootstrap/tree_menu_right.html', 'Tree menu right'),
     ('pylucid/simple.html', 'Simple'),
+    ('base.html', 'Basic Page'),
 )
 
-CMS_PERMISSION = True
+# http://docs.django-cms.org/en/latest/reference/configuration.html#cms-permission
+CMS_PERMISSION = False
 
-# http://django-cms.readthedocs.org/en/support-3.0.x/reference/configuration.html#cms-placeholder-conf
+# Basic Placeholder config
+
+# from djangocms_text_ckeditor.cms_plugins import TextPlugin
+CKEDITOR = "TextPlugin"
+
+from django_cms_tools.plugin_anchor_menu import constants as plugin_anchor_menu_constants
+
 CMS_PLACEHOLDER_CONF = {
-    'content': {
-        'name' : _('Content'),
-
-        # list of plugins that can be added to this placeholder. If not supplied, all plugins can be selected:
-        #'plugins': ['TextPlugin', 'LinkPlugin'],
-
-        # list of default plugins which will be automagically added when the placeholder will be created:
-        'default_plugins':[
+    None: {
+        'name': _("Content"),
+        'plugins': [
+            CKEDITOR,
+            plugin_anchor_menu_constants.ANCHOR_PLUGIN_NAME,
+            plugin_anchor_menu_constants.DROP_DOWN_ANCHOR_MENU_PLUGIN_NAME,
+        ],
+        'default_plugins': [
             {
-                'plugin_type':'TextPlugin',
+                'plugin_type': CKEDITOR,
                 'values':{'body':'<p>(There is no content, yet.)</p>'},
             },
-        ]
+        ],
     },
-    'post_content': {
-        'name' : _('Blog Content'),
-        # list of default plugins which will be automagically added when the placeholder will be created:
-        'default_plugins':[
-            {
-                'plugin_type':'TextPlugin',
-                'values':{'body':'<p>(There is no content, yet.)</p>'},
-            },
-        ]
-    },
-}
-
-
-
-MIGRATION_MODULES = {
-    'djangocms_text_ckeditor': 'djangocms_text_ckeditor.migrations',
-    'cmsplugin_markup': 'cmsplugin_markup.migrations_django',
-}
-
-
-# django-reversion-compare settings:
-ADD_REVERSION_ADMIN=True # Add the reversion modes to admin interface
-
-
-# django-debug-toolbar settings:
-DEBUG_TOOLBAR_PATCH_SETTINGS = False
-DEBUG_TOOLBAR_PANELS = [
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
-
-    # https://github.com/jedie/django-debug-toolbar-django-info
-    'django_info_panel.panels.database.DatabaseInfo',
-    'django_info_panel.panels.urlpatterns.UrlPatternsInfo',
-    'django_info_panel.panels.template.TemplateInfo',
-]
-
-
-# https://github.com/nephila/djangocms-blog#quick-hint
-SOUTH_MIGRATION_MODULES = {
-    'easy_thumbnails': 'easy_thumbnails.south_migrations',
-    'taggit': 'taggit.south_migrations',
 }
 
 
@@ -258,29 +303,72 @@ THUMBNAIL_PROCESSORS = (
 )
 
 
-# https://github.com/nephila/django-meta#configuration
-META_SITE_PROTOCOL = "http" # This should be set to either 'http' or 'https'
-META_USE_SITES = True # use Django's sites contrib app
+#_____________________________________________________________________________
+# cut 'pathname' in log output
+
+import logging
+
+old_factory = logging.getLogRecordFactory()
 
 
-# https://github.com/mitar/cmsplugin-markup
-CMS_MARKUP_OPTIONS = (
-    'cmsplugin_markup.plugins.creole',
-    'cmsplugin_markup.plugins.html',
-    'cmsplugin_markup.plugins.markdown',
-    'cmsplugin_markup.plugins.textile',
-    'cmsplugin_markup.plugins.restructuredtext',
-)
-CMS_MARKUP_RENDER_ALWAYS = True
-CMS_MARKDOWN_EXTENSIONS = ()
-
-# DEBUG_TOOLBAR_CONFIG = {
-#     # For local debugging: print information if IP address not in INTERNAL_IPS
-#     "SHOW_TOOLBAR_CALLBACK":"pylucid_debug.debug_toolbar_helper.show_toolbar",
-# }
+def cut_path(pathname, max_length):
+    if len(pathname)<=max_length:
+        return pathname
+    return "...%s" % pathname[-(max_length-3):]
 
 
+def record_factory(*args, **kwargs):
+    record = old_factory(*args, **kwargs)
+    record.cut_path = cut_path(record.pathname, 30)
+    return record
 
-LOGIN_REDIRECT_URL="/"
-LOGIN_URL="/admin/login/"
-LOGOUT_URL="/admin/logout/"
+
+logging.setLogRecordFactory(record_factory)
+
+
+# -----------------------------------------------------------------------------
+
+
+# https://docs.python.org/3/library/logging.html#logging-levels
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)8s %(cut_path)s:%(lineno)-3s %(message)s'
+        },
+    },
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose',
+        }
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+        'django_tools': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'django_cms_tools': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+        'pylucid': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    },
+}
