d5a9eba2fd388a2add17d16ed4e3b8bedc4cfaf2
JensDiemer <git@jensdiemer.de>
2018-03-06 20:14:16 +0100

    deactivate caching

--- pylucid_page_instance/settings.py
+++ pylucid_page_instance/settings.py
@@ -18,8 +18,14 @@ SECRET_KEY = 'Only for the tests ;)'
 
 DEBUG = True
 
-
 TEMPLATES[0]["DIRS"] = [str(Path(BASE_DIR, "templates/"))]
+TEMPLATES[0]["OPTIONS"]["debug"] = DEBUG
+
+# Don't cache template loading:
+TEMPLATES[0]["OPTIONS"]['loaders']= [
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+]
 
 
 STATIC_ROOT = str(Path(BASE_DIR, 'static'))
@@ -32,3 +38,15 @@ DATABASES = {
         'NAME': str(Path(BASE_DIR, '..', 'test_project_db.sqlite3').resolve()),
     }
 }
+
+# Deactivate caches
+# https://docs.djangoproject.com/en/1.11/topics/cache/#dummy-caching-for-development
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.dummy.DummyCache',
+    }
+}
+
+# https://django-compressor.readthedocs.io/en/latest/settings/
+# COMPRESS_ENABLED=False
+COMPRESS_ENABLED=True
