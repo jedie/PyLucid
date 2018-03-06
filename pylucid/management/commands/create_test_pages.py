a8261a8e3442a836c837bcd26d0fecb734be48b7
JensDiemer <git@jensdiemer.de>
2018-03-06 20:13:28 +0100

    create dummy pages while running "run_test_project_dev_server"

--- pylucid/management/commands/create_test_pages.py
+++ pylucid/management/commands/create_test_pages.py
@@ -4,7 +4,19 @@ from django.conf import settings
 from django.core.management import BaseCommand
 
 # https://github.com/jedie/django-cms-tools
-from django_cms_tools.fixtures.pages import CmsPageCreator
+from django_cms_tools.fixtures.pages import CmsPageCreator, DummyPageGenerator
+
+
+class SubPageGenerator(DummyPageGenerator):
+    def __init__(self, parent_page, *args, **kwargs):
+        self.parent_page = parent_page
+
+        super().__init__(*args, **kwargs)
+
+    def get_parent_page(self):
+        if self.current_level == 1:
+            return self.parent_page
+        return super().get_parent_page()
 
 
 class TestPageCreator(CmsPageCreator):
@@ -21,4 +33,6 @@ class TestPageCreator(CmsPageCreator):
 class Command(BaseCommand):
     def handle(self, **options):
         for template_file, template_name in settings.CMS_TEMPLATES:
-            TestPageCreator(template_file, template_name).create()
+            page, created = TestPageCreator(template_file, template_name).create()
+            if created:
+                SubPageGenerator(parent_page=page, title_prefix=template_name).create()
