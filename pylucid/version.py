5ce1945fe8e50ecba702ce1d722a251dce96e35f
JensDiemer <git@jensdiemer.de>
2018-04-03 18:34:00 +0200

    release as v3.1.2

--- pylucid/version.py
+++ pylucid/version.py
@@ -14,7 +14,7 @@
 #   * Set pip '--pre' flag in pylucid_boot.NORMAL_INSTALL
 #   * push not pre-releases on 'master' branch
 #
-__version__ = "3.1.2.dev1"
+__version__ = "3.1.2"
 
 
 # Just the first two parts of the version
