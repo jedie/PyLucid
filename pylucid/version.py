3a2aa185f3cfb868116154420d78d1ba54a23f1f
JensDiemer <git@jensdiemer.de>
2018-04-24 10:07:15 +0200

    add bootstrap-env to normal requirements, too

--- pylucid/version.py
+++ pylucid/version.py
@@ -14,7 +14,7 @@
 #   * Set pip '--pre' flag in pylucid_boot.NORMAL_INSTALL
 #   * push not pre-releases on 'master' branch
 #
-__version__ = "3.1.3rc1"
+__version__ = "3.1.3rc2"
 
 
 # Just the first two parts of the version
