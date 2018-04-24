935c367b26e2ff9ef5d4fb1faf24b55961b7a971
JensDiemer <git@jensdiemer.de>
2018-04-24 09:37:41 +0200

    update boot file

--- pylucid/pylucid_boot.py
+++ pylucid/pylucid_boot.py
@@ -46,7 +46,7 @@ import time
 import traceback
 from pathlib import Path
 
-if sys.version_info < (3, 5):  # isort:skip
+if sys.version_info < (3, 5):
     print("\nERROR: Python 3.5 or greater is required!")
     print("(Current Python Verison is %s)\n" % sys.version.split(" ",1)[0])
     sys.exit(101)
@@ -58,6 +58,7 @@ except ImportError as err:
     # e.g.: debian / ubuntu doesn't have venv installed, isn't it?!?
     print("\nERROR: 'venv' not available: %s (Maybe 'python3-venv' package not installed?!?)" % err)
 
+
 try:
     import ensurepip
 except ImportError as err:
@@ -65,7 +66,7 @@ except ImportError as err:
     print("\nERROR: 'ensurepip' not available: %s (Maybe 'python3-venv' package not installed?!?)" % err)
 
 
-__version__ = "1.0.0rc14" # Version from used 'bootstrap_env' to generate this file.
+__version__ = "1.0.0rc17" # Version from used 'bootstrap_env' to generate this file.
 
 
 log = logging.getLogger(__name__)
@@ -87,6 +88,7 @@ ADMIN_FILE_NAME="pylucid_admin.py" # File under .../<project>/foobar_admin.py
 #
 DEVELOPER_INSTALL=["-e", "git+https://github.com/jedie/PyLucid.git@master#egg=%s" % PACKAGE_NAME]
 NORMAL_INSTALL=[
+    "--pre", # https://pip.pypa.io/en/stable/reference/pip_install/#pre-release-versions
     PACKAGE_NAME
 ]
 
@@ -120,7 +122,7 @@ class Colorizer:
     https://github.com/django/django/blob/master/django/utils/termcolors.py
 
     >>> c = Colorizer()
-    >>> c.supports_colors()
+    >>> c._supports_colors()
     True
     >>> c.color_support = True
     >>> c.colorize('no color')
@@ -358,15 +360,22 @@ def get_pip_file_name():
         return "pip3"
 
 
-def display_errors(func):
-    def wrapped(*args, **kwargs):
+
+class DisplayErrors:
+    """
+    Decorator to print traceback on exceptions.
+    Used in e.g.: Cmd class
+    """
+    def __init__(self, func):
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            self.func(*args, **kwargs)
         except Exception as err:
             traceback.print_exc(file=sys.stderr)
             return "%s: %s" % (err.__class__.__name__, err)
 
-    return wrapped
 
 
 class Cmd2(cmd.Cmd):
@@ -429,14 +438,14 @@ class Cmd2(cmd.Cmd):
         """ Called on an input line when the command prefix is not recognized. """
         colorizer.err(self.unknown_command % line, foreground="red")
 
-    @display_errors
+    @DisplayErrors
     def _complete_list(self, items, text, line, begidx, endidx):
         if text:
             return [x for x in items if x.startswith(text)]
         else:
             return items
 
-    @display_errors
+    @DisplayErrors
     def _complete_path(self, text, line, begidx, endidx):
         """
         complete a command argument with a existing path
@@ -548,6 +557,12 @@ class Cmd2(cmd.Cmd):
 
 
 class EnvBuilder(venv.EnvBuilder):
+    """
+    * Create new virtualenv
+    * install and update pip
+    * install "pylucid"
+    * call "pylucid_admin.py update_env" to install all requirements
+    """
     verbose = True
 
     def __init__(self, requirements):
@@ -632,7 +647,7 @@ class EnvBuilder(venv.EnvBuilder):
         """
         print(" * post-setup modification")
 
-        # Install bootstrap_env
+        # Install "pylucid"
         #   in normal mode as package from PyPi
         #   in dev. mode as editable from github
         self.call_new_python(
@@ -642,26 +657,31 @@ class EnvBuilder(venv.EnvBuilder):
             *self.requirements
         )
 
-        # Check if ".../bin/bootstrap_env_admin.py" exists
+        # Check if ".../bin/pylucid_admin.py" exists
         bootstrap_env_admin_path = Path(context.bin_path, ADMIN_FILE_NAME)
         if not bootstrap_env_admin_path.is_file():
             print("ERROR: admin script not found here: '%s'" % bootstrap_env_admin_path)
             VerboseSubprocess("ls", "-la", str(context.bin_path)).verbose_call()
             sys.exit(-1)
 
-        # Install all requirements
+        # Install all requirements by call: "pylucid_admin.py update_env"
         self.call_new_python(
             context,
             context.env_exe,
             str(bootstrap_env_admin_path),
             "update_env",
-            timeout=240
+            timeout=4*60
         )  # extended timeout for slow Travis ;)
 
 
 
-
 class BootBootstrapEnvShell(Cmd2):
+    """
+    The bootstrap shell to start the virtualenv creation.
+    It's implement only two commands:
+     * boot
+     * boot_developer
+    """
     def _resolve_path(self, path):
         return Path(path).expanduser().resolve()
 
@@ -743,9 +763,6 @@ class BootBootstrapEnvShell(Cmd2):
     complete_boot_developer = complete_boot
 
 
-def main():
-    BootBootstrapEnvShell().cmdloop()
-
-
 if __name__ == '__main__':
-    main()
+    # Start the shell
+    BootBootstrapEnvShell().cmdloop()
