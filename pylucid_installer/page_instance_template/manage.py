15295e9d57acd23682a892181ffd133d42060493
JensDiemer <git@jensdiemer.de>
2019-01-28 18:27:26 +0100

    check Python version

--- pylucid_installer/page_instance_template/manage.py
+++ pylucid_installer/page_instance_template/manage.py
@@ -3,6 +3,12 @@
 import os
 import sys
 
+if sys.version_info < (3, 5):
+    print("\nERROR: Python 3.5 or greater is required!\n")
+    print("(Current Python Version is %s)\n" % sys.version.split(" ", 1)[0])
+    sys.exit(101)
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_project.settings")
     # print("Use DJANGO_SETTINGS_MODULE=%r" % os.environ["DJANGO_SETTINGS_MODULE"])
